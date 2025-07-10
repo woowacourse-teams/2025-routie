e8436293f2c07e59dbcb13439e4bce66dbf28439
KimDaehyeon <daehyeon3351@gmail.com>
2025-07-10 21:03:03 +0900

    chore: 백엔드 프로젝트 초기 세팅

---
+++ backend/spring-routie/src/test/java/routie/RoutieApplicationTests.java
@@ -0,0 +1,13 @@
+package routie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
