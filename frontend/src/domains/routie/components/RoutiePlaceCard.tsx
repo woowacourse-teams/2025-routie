5a1a593953bacb36b21038217f2b6354ea18b36e
aydenote <dslgpgh@gmail.com>
2025-07-23 00:27:46 +0900

    refactor: props 타입 수정. 이중 객체 제거

--- frontend/src/domains/routie/components/RoutiePlaceCard.tsx
+++ frontend/src/domains/routie/components/RoutiePlaceCard.tsx
@@ -1,4 +1,3 @@
-
 import Card from '@/@common/components/Card/Card';
 import Flex from '@/@common/components/Flex/Flex';
 import IconButton from '@/@common/components/IconButton/IconButton';
@@ -9,33 +8,31 @@ import trashIcon from '@/assets/icons/trash.svg';
 import theme from '@/styles/theme';
 
 interface RoutiePlaceCardProps {
-  place: {
     id: string;
     name: string;
     address: string;
     stay_duration_minutes:number;
     open_at:string;
     close_at:string
-  };
 }
 
-const RoutiePlaceCard = ({place}:RoutiePlaceCardProps) => {
+const RoutiePlaceCard = ({...props}:RoutiePlaceCardProps) => {
   return (
-    <Card id={place.id} width="45rem" variant="defaultStatic" >
+    <Card id={props.id} width="45rem" variant="defaultStatic" >
       <Flex justifyContent="flex-start" gap={1.5}>
         <IconButton variant='drag' icon={dragIcon} onClick={()=>{}}></IconButton>
         <Flex direction="column" alignItems="flex-start" gap={1.1} width="100%">
           <Flex width="100%" justifyContent="space-between">
-            <Text variant="caption">{place.name}</Text>
+            <Text variant="caption">{props.name}</Text>
             <Flex gap={0.4}>
-              <Pill variant='filled' type="time">{place.stay_duration_minutes}분</Pill>
+              <Pill variant='filled' type="time">{props.stay_duration_minutes}분</Pill>
               <IconButton icon={trashIcon} variant="delete" onClick={() => {}} />
             </Flex>
           </Flex>
           <Text variant="label" color={theme.colors.gray[200]}>
-            {place.address}
+            {props.address}
           </Text>
-            <Pill type="time">{place.open_at.slice(0,5)}-{place.close_at.slice(0,5)}</Pill>
+            <Pill type="time">{props.open_at.slice(0,5)}-{props.close_at.slice(0,5)}</Pill>
         </Flex>
       </Flex>
     </Card>
