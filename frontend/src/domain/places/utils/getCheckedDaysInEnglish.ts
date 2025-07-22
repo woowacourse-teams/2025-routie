8aa9273c04a07c419815264888e008f922794144
youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-22 15:58:04 +0900

    refactor: 선택 해제된 휴무일을 return 하도록 수정

--- frontend/src/domain/places/utils/getCheckedDaysInEnglish.ts
+++ frontend/src/domain/places/utils/getCheckedDaysInEnglish.ts
@@ -2,7 +2,7 @@ import { DAY_KR_LIST, DAY_KR_TO_EN } from '../constants/day';
 
 export const getCheckedDaysInEnglish = (checkedList: boolean[]) => {
   return checkedList
-    .map((isChecked, index) => (isChecked ? DAY_KR_LIST[index] : null))
+    .map((isChecked, index) => (!isChecked ? DAY_KR_LIST[index] : null))
     .filter((day): day is (typeof DAY_KR_LIST)[number] => day !== null)
     .map((krDay) => DAY_KR_TO_EN[krDay]);
 };
