46d58656a9fe4de3ab754b57fdd3bd89cf9b9786
KimDaehyeon <daehyeon3351@gmail.com>
2025-07-22 17:47:27 +0900

    feat: 루티 스페이스 생성 기능 구현

---
+++ backend/spring-routie/src/main/java/routie/routiespace/service/RoutieSpaceService.java
@@ -0,0 +1,22 @@
+package routie.routiespace.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import routie.routiespace.domain.RoutieSpace;
+import routie.routiespace.domain.RoutieSpaceIdentifierProvider;
+import routie.routiespace.repository.RoutieSpaceRepository;
+
+@Service
+@RequiredArgsConstructor
+public class RoutieSpaceService {
+
+    private final RoutieSpaceRepository routieSpaceRepository;
+    private final RoutieSpaceIdentifierProvider routieSpaceIdentifierProvider;
+
+    @Transactional
+    public RoutieSpace add() {
+        RoutieSpace routieSpace = RoutieSpace.from(routieSpaceIdentifierProvider);
+        return routieSpaceRepository.save(routieSpace);
+    }
+}
