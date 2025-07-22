75169797d633045c463168b6733f8fc7dfa65641
KimDaehyeon <daehyeon3351@gmail.com>
2025-07-22 17:47:42 +0900

    feat: 루티 스페이스 생성 api 구현

---
+++ backend/spring-routie/src/main/java/routie/routiespace/controller/RoutieSpaceController.java
@@ -0,0 +1,27 @@
+package routie.routiespace.controller;
+
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import routie.routiespace.domain.RoutieSpace;
+import routie.routiespace.service.RoutieSpaceService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/routie-spaces")
+public class RoutieSpaceController {
+
+    private final RoutieSpaceService routieSpaceService;
+
+    @PostMapping
+    public ResponseEntity<Void> createRoutieSpace() {
+        RoutieSpace routieSpace = routieSpaceService.add();
+        URI location = URI.create(String.format("/routie-spaces/%s", routieSpace.getIdentifier()));
+
+        return ResponseEntity.created(location).build();
+    }
+}
+
