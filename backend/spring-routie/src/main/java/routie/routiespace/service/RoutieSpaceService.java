47b5498d5862f0c0d4dc61a51b4e060e61ab3dc7
KimDaehyeon <daehyeon3351@gmail.com>
2025-07-22 20:55:06 +0900

    feat: 루티 스페이스 이름 변경 API 구현

--- backend/spring-routie/src/main/java/routie/routiespace/service/RoutieSpaceService.java
+++ backend/spring-routie/src/main/java/routie/routiespace/service/RoutieSpaceService.java
@@ -3,6 +3,8 @@ package routie.routiespace.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import routie.routiespace.controller.dto.request.UpdateRoutieSpaceNameRequest;
+import routie.routiespace.controller.dto.response.UpdateRoutieSpaceNameResponse;
 import routie.routiespace.domain.RoutieSpace;
 import routie.routiespace.domain.RoutieSpaceIdentifierProvider;
 import routie.routiespace.repository.RoutieSpaceRepository;
@@ -18,5 +20,19 @@ public class RoutieSpaceService {
     public RoutieSpace addRoutieSpace() {
         RoutieSpace routieSpace = RoutieSpace.from(routieSpaceIdentifierProvider);
         return routieSpaceRepository.save(routieSpace);
+    }
+
+    @Transactional
+    public UpdateRoutieSpaceNameResponse updateRoutieSpaceName(
+            final String routieSpaceIdentifier,
+            final UpdateRoutieSpaceNameRequest updateRoutieSpaceNameRequest
+    ) {
+        // TODO: 예외처리 구조 개선 예정
+        RoutieSpace routieSpace = routieSpaceRepository.findByIdentifier(routieSpaceIdentifier)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 루티 스페이스입니다."));
+
+        routieSpace.updateName(updateRoutieSpaceNameRequest.name());
+
+        return new UpdateRoutieSpaceNameResponse(routieSpace.getName());
     }
 }
