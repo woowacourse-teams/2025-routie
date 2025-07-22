4f1fad48fefe8feb5dc17bf1534b1813679992c5
youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-22 15:22:41 +0900

    feat: DateCheckboxList 컴포넌트 구현

---
+++ frontend/src/domain/places/components/DateCheckboxList/DateCheckboxList.tsx
@@ -0,0 +1,31 @@
+import Flex from '@/@common/components/Flex/Flex';
+import { DateType } from '@/domain/places/types/date.types';
+
+import { DAY_KR_LIST } from '../../constants/day';
+import DateCheckbox from '../DateCheckbox/DateCheckbox';
+
+interface DateCheckboxListProps {
+  value: boolean[];
+  onChange: (index: number) => void;
+}
+
+const DateCheckboxList = ({
+  value: dateCheckedList,
+  onChange: handleCheckboxChange,
+}: DateCheckboxListProps) => {
+  return (
+    <Flex direction="row" alignItems="center" justifyContent="center" gap={1}>
+      {DAY_KR_LIST.map((day, index) => (
+        <DateCheckbox
+          key={day}
+          id={day}
+          label={day as DateType}
+          isChecked={dateCheckedList[index]}
+          onChange={() => handleCheckboxChange(index)}
+        />
+      ))}
+    </Flex>
+  );
+};
+
+export default DateCheckboxList;
