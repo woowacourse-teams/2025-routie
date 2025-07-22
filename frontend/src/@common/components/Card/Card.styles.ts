6e3f2b31888f7de117e795be8d3e25d6ea1a9e20
Seungsoo Choi <77476077+aydenote@users.noreply.github.com>
2025-07-23 01:24:27 +0900

    feat: 루티 장소 컴포넌트 구현 (#138)

--- frontend/src/@common/components/Card/Card.styles.ts
+++ frontend/src/@common/components/Card/Card.styles.ts
@@ -16,6 +16,13 @@ const cardVariant: Record<CardVariantProps, SerializedStyles> = {
     }
   `,
 
+  defaultStatic: css`
+    padding: 0.8rem;
+    border: 1px solid ${theme.colors.purple[50]};
+    border-left: 4px solid ${theme.colors.purple[100]};
+    background-color: ${theme.colors.white};
+  `,
+
   available: css`
     border-left: 4px solid ${theme.colors.green[100]};
     background-color: ${theme.colors.green[50]};
