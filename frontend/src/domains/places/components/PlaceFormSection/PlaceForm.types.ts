85c51d243037981faaeb861547b1d0c1ac1030de
youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-23 09:58:38 +0900

    chore: 불필요한 주석 삭제

--- frontend/src/domains/places/components/PlaceFormSection/PlaceForm.types.ts
+++ frontend/src/domains/places/components/PlaceFormSection/PlaceForm.types.ts
@@ -15,5 +15,5 @@ export type FormAction =
       field: keyof Omit<FormState, 'closedDays'>;
       value: string;
     }
-  | { type: 'TOGGLE_DAY'; index: number } // ✅ 요일 토글 전용 액션
+  | { type: 'TOGGLE_DAY'; index: number }
   | { type: 'RESET' };
