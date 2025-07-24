82b0d03921a727e0895cb7ca110f5076b0db6fb1
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-24 15:12:14 +0900

    refactor: 폼 제출 시 모달 닫히도록 수정, UI 중복 요소 제거, 휴무일 데이터 서버 전송 전 영문 요일 리스트로 변환 (#168)

--- frontend/src/domains/places/components/AddPlaceModal/AddPlaceModalButtons.tsx
+++ frontend/src/domains/places/components/AddPlaceModal/AddPlaceModalButtons.tsx
@@ -2,20 +2,9 @@ import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
 import Text from '@/@common/components/Text/Text';
 
-interface AddPlaceModalButtonsProps {
-  onClose: () => void;
-}
-
-const AddPlaceModalButtons = ({ onClose }: AddPlaceModalButtonsProps) => {
+const AddPlaceModalButtons = () => {
   return (
     <Flex justifyContent="space-between" width="100%" gap={1.6}>
-      <Button type="button" variant="secondary">
-        <Flex width="100%">
-          <Text variant="caption" color="purple" onClick={onClose}>
-            닫기
-          </Text>
-        </Flex>
-      </Button>
       <Button type="submit" variant="primary">
         <Flex width="100%">
           <Text variant="caption" color="white">
