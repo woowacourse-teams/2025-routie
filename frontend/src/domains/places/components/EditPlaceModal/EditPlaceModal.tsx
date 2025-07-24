235c9208aa0460316e9fbe13167af2c91b0f70bf
youjeong Jeong <wjddbwjd1205@naver.com>
2025-07-24 22:51:06 +0900

    fix: 의존성 배열에서 initializeForm을 제거하여 무한 렌더링 문제 해결

--- frontend/src/domains/places/components/EditPlaceModal/EditPlaceModal.tsx
+++ frontend/src/domains/places/components/EditPlaceModal/EditPlaceModal.tsx
@@ -37,7 +37,7 @@ const EditPlaceModal = ({ isOpen, onClose, id }: EditPlaceModalProps) => {
     };
 
     fetchData();
-  }, [isOpen, id, initializeForm]);
+  }, [isOpen, id]);
 
   const handleClose = () => {
     resetForm();
