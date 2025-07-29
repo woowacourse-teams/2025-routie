06b0d3b98c79287f813657579f6dcb234b7f48e7
KimDaehyeon <daehyeon3351@gmail.com>
2025-07-29 10:03:17 +0900

    refactor: HttpHeaders를 사용하도록 변경

--- backend/spring-routie/src/main/java/routie/auth/config/WebMvcConfig.java
+++ backend/spring-routie/src/main/java/routie/auth/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package routie.auth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,14 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Profile({"local", "dev"})
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    private static final String LOCATION_HEADER_NAME = "Location";
-
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .exposedHeaders(LOCATION_HEADER_NAME);
+                .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
