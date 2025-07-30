ae737ae50d6c6cfdb0299fe711d9143d3d4a8a68
goohong <goohong.chung@gmail.com>
2025-07-30 17:29:16 +0900

    refactor: ValidationStrategy에 실패 메세지 필드 추가

--- backend/spring-routie/src/main/java/routie/routie/domain/ValidationStrategy.java
+++ backend/spring-routie/src/main/java/routie/routie/domain/ValidationStrategy.java
@@ -1,5 +1,16 @@
 package routie.routie.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ValidationStrategy {
-    IS_NOT_CLOSED_DAY, IS_WITHIN_OPERATION_HOURS, IS_NOT_DURING_BREAKTIME
+
+    IS_WITHIN_TOTAL_TIME("장소 방문 종료 시각이 루티 종료 시각을 초과합니다"),
+    IS_NOT_CLOSED_DAY("휴무일에 방문하는 장소가 있습니다"),
+    IS_WITHIN_OPERATION_HOURS("운영 시간 외에 방문하는 장소가 있습니다"),
+    IS_NOT_DURING_BREAKTIME("브레이크 타임에 방문하는 장소가 있습니다");
+
+    private final String failMessage;
 }
