6231f1b3b98fd0e9072ecc91a6bf0364b7bab750
ohgus <123801385+ohgus@users.noreply.github.com>
2025-07-31 13:19:27 +0900

    refactor: 일정 시작, 종료 시간 입력 제한 추가 (#300)

--- frontend/src/layouts/Sidebar/TimeInput.tsx
+++ frontend/src/layouts/Sidebar/TimeInput.tsx
@@ -1,5 +1,6 @@
 import Flex from '@/@common/components/Flex/Flex';
 import Input from '@/@common/components/Input/Input';
+import { getKoreanDateTimeISO } from '@/@common/utils/format';
 
 const TimeInput = ({
   time,
@@ -8,6 +9,8 @@ const TimeInput = ({
   time: { startDateTime: string; endDateTime: string };
   onChange: (field: 'startDateTime' | 'endDateTime', value: string) => void;
 }) => {
+  const minimumStartDateTime = getKoreanDateTimeISO(new Date());
+
   return (
     <Flex justifyContent="space-between" width="100%" gap={1}>
       <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
@@ -16,15 +19,19 @@ const TimeInput = ({
           label="시작"
           type="datetime-local"
           value={time.startDateTime}
+          min={minimumStartDateTime}
           onChange={(value) => onChange('startDateTime', value)}
         />
       </Flex>
       <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
         <Input
+          disabled={time.startDateTime === ''}
           id="endAt"
           label="종료"
           type="datetime-local"
           value={time.endDateTime}
+          min={time.startDateTime ? time.startDateTime : minimumStartDateTime}
+          max={time.startDateTime ? time.startDateTime : minimumStartDateTime}
           onChange={(value) => onChange('endDateTime', value)}
         />
       </Flex>
