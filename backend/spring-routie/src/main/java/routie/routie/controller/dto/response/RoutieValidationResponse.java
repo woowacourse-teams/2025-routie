5fd5052663e68fac0c5b1f61a7f3754f1f8b0e6b
goohong <goohong.chung@gmail.com>
2025-08-01 17:13:40 +0900

    refactor: 검증 로직 Response 수정

--- backend/spring-routie/src/main/java/routie/routie/controller/dto/response/RoutieValidationResponse.java
+++ backend/spring-routie/src/main/java/routie/routie/controller/dto/response/RoutieValidationResponse.java
@@ -1,6 +1,30 @@
 package routie.routie.controller.dto.response;
 
+import java.util.List;
+import routie.routie.domain.routievalidator.ValidationResult;
+
 public record RoutieValidationResponse(
-        boolean isValid
+        List<ValidationResultResponse> validationResultResponses
 ) {
+    public static RoutieValidationResponse from(final List<ValidationResult> validationResults) {
+        return new RoutieValidationResponse(
+                validationResults.stream()
+                        .map(ValidationResultResponse::from)
+                        .toList()
+        );
+    }
+
+    public record ValidationResultResponse(
+            boolean isValid,
+            String strategy,
+            String message
+    ) {
+        public static ValidationResultResponse from(final ValidationResult validationResult) {
+            return new ValidationResultResponse(
+                    validationResult.isValid(),
+                    validationResult.strategy(),
+                    validationResult.message()
+            );
+        }
+    }
 }
