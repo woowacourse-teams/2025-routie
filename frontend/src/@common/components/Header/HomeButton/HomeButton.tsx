e714d6d9642b05d28240b9f6770c98099bfa17e2
AHHYUNJU <wndkgus0225@naver.com>
2025-08-01 14:29:36 +0900

    refactor: 홈 버튼에서 로고와 텍스트 모두 클릭 시 홈 화면으로 이동하도록 수정

--- frontend/src/@common/components/Header/HomeButton/HomeButton.tsx
+++ frontend/src/@common/components/Header/HomeButton/HomeButton.tsx
@@ -1,5 +1,6 @@
 import theme from '@/styles/theme';
 
+import Flex from '../../Flex/Flex';
 import Text from '../../Text/Text';
 
 type HomeButtonProps = {
@@ -11,11 +12,13 @@ const HomeButton = ({ icon, onClick, ...props }: HomeButtonProps) => {
   return (
     <>
       <button onClick={onClick} {...props}>
-        <img src={icon} />
+        <Flex gap={1}>
+          <img src={icon} />
+          <Text variant="title" color={theme.colors.purple[400]}>
+            Routie
+          </Text>
+        </Flex>
       </button>
-      <Text variant="title" color={theme.colors.purple[400]}>
-        Routie
-      </Text>
     </>
   );
 };
