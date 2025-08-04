46d46d2a2fd93bb03b279ffd4b501fa53fcc45d8
AHHYUNJU <wndkgus0225@naver.com>
2025-08-04 15:17:14 +0900

    feat: EmptyRoutieMessage 컴포넌트 스타일 개선

--- frontend/src/domains/routie/components/EmptyRoutieMessage/EmptyRoutieMessage.tsx
+++ frontend/src/domains/routie/components/EmptyRoutieMessage/EmptyRoutieMessage.tsx
@@ -1,10 +1,13 @@
 import Flex from '@/@common/components/Flex/Flex';
 import Text from '@/@common/components/Text/Text';
+import theme from '@/styles/theme';
 
 const EmptyRoutieMessage = () => (
-  <Flex width="100%" padding={8} style={{ textAlign: 'center' }}>
-    <Text variant="subTitle">
-      동선이 생성되지 않았어요. <br />
+  <Flex width="100%" direction="column" padding={8} gap={1}>
+    <Text variant="subTitle" color={theme.colors.gray[200]}>
+      동선이 생성되지 않았어요.
+    </Text>
+    <Text variant="subTitle" color={theme.colors.gray[200]}>
       장소를 2개 이상 추가하면 동선이 생성됩니다!
     </Text>
   </Flex>
