669d3d2a28952c1bed8da52c02fcbfbc4a8c852a
ohgus <anakin1108@naver.com>
2025-08-04 17:03:41 +0900

    chore: GA 초기 세팅

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,16 +1,30 @@
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
+import { useGoogleAnalytics } from '@/libs/googleAnalytics/hooks/useGoogleAnalytics';
 import Home from '@/pages/Home/Home';
 import RoutieSpace from '@/pages/RoutieSpace/RoutieSpace';
+
+const LayoutWithAnalytics = ({ children }: { children: React.ReactNode }) => {
+  useGoogleAnalytics();
+  return <>{children}</>;
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: (
+      <LayoutWithAnalytics>
+        <Home />
+      </LayoutWithAnalytics>
+    ),
   },
   {
     path: '/routie-spaces',
-    element: <RoutieSpace />,
+    element: (
+      <LayoutWithAnalytics>
+        <RoutieSpace />
+      </LayoutWithAnalytics>
+    ),
   },
 ]);
 
