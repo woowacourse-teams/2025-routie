e5fcfc8803ab75f7d25fe9b2bb273a10577c2a6b
AHHYUNJU <wndkgus0225@naver.com>
2025-08-05 16:53:58 +0900

    chore: sentry 초기 세팅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,15 @@
+import * as Sentry from '@sentry/react';
 import { createRoot } from 'react-dom/client';
 
 import './styles/reset.css';
 import './styles/font.css';
 
 import Route from './routes';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  sendDefaultPii: true,
+});
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<Route />);
