37284a242601c45ba364ac69b0aeb5ff93243534
AHHYUNJU <wndkgus0225@naver.com>
2025-08-05 17:36:30 +0900

    chore: Sentry trace 설정 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,6 +9,9 @@ import Route from './routes';
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
   sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracePropagationTargets: ['localhost', 'https://api.routie.me'],
+  tracesSampleRate: 1.0,
 });
 
 const root = createRoot(document.getElementById('root')!);
