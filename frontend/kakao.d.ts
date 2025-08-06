74316d5147b6eb1cb42a425f4b8cd5203d9ef327
ohgus <anakin1108@naver.com>
2025-08-06 19:15:06 +0900

    chore: KakaoMap 도입을 위한 초기세팅

---
+++ frontend/kakao.d.ts
@@ -0,0 +1,29 @@
+declare global {
+  interface Window {
+    kakao: {
+      maps: {
+        load: (callback: () => void) => void;
+        LatLng: new (lat: number, lng: number) => any;
+        Map: new (container: HTMLElement, options: any) => any;
+        Marker: new (options: any) => any;
+        LatLngBounds: new () => any;
+        Polyline: new (options: any) => any;
+        InfoWindow: new (options: any) => any;
+        event: {
+          addListener: (target: any, type: string, handler: () => void) => void;
+        };
+        RoutePriority: {
+          WALKING: any;
+        };
+        services: {
+          DirectionService: new () => any;
+          Status: {
+            OK: any;
+          };
+        };
+      };
+    };
+  }
+}
+
+export {};
