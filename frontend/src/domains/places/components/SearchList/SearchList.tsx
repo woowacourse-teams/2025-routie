f92dac35fbb782d74db7d5bf34ef1b7684fc1a65
aydenote <dslgpgh@gmail.com>
2025-08-06 21:10:06 +0900

    feat: 장소 검색 결과 컴포넌트 구현

---
+++ frontend/src/domains/places/components/SearchList/SearchList.tsx
@@ -0,0 +1,45 @@
+import Button from '@/@common/components/Button/Button';
+import Flex from '@/@common/components/Flex/Flex';
+import Text from '@/@common/components/Text/Text';
+import pinIcon from '@/assets/icons/pin.svg';
+
+import { PlaceSearchType } from '../../types/place.types';
+import { listStyle, itemButtonStyle } from '../SearchBox/SearchBox.styles';
+
+interface SearchListProps {
+  searchResults: PlaceSearchType[];
+  onClick: (item: PlaceSearchType) => void;
+}
+
+const SearchList = ({ searchResults, onClick }: SearchListProps) => {
+  return (
+    <ul css={listStyle}>
+      {searchResults.map((searchResult) => (
+        <li key={searchResult.placeName}>
+          <Button
+            type="button"
+            variant="secondary"
+            width="100%"
+            css={itemButtonStyle}
+            onClick={() => onClick(searchResult)}
+          >
+            <Flex alignItems="center" gap={1}>
+              <img src={pinIcon} alt="pin" />
+              <Flex
+                direction="column"
+                alignItems="flex-start"
+                gap={0.5}
+                width="100%"
+              >
+                <Text variant="caption">{searchResult.placeName}</Text>
+                <Text variant="label">{searchResult.roadAddressName}</Text>
+              </Flex>
+            </Flex>
+          </Button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SearchList;
