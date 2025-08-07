c9d4f2d93e8e41b513a5e21635097aa68f895a2e
머랭 <113977176+cookie-meringue@users.noreply.github.com>
2025-08-07 14:46:30 +0900

    feat: 프로덕션 환경 cors 설정 추가 (#419)

--- backend/spring-routie/src/main/java/routie/auth/config/ProdCorsConfig.java
+++ backend/spring-routie/src/main/java/routie/auth/config/ProdCorsConfig.java
@@ -2,20 +2,23 @@ package routie.auth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Profile({"local", "dev"})
-public class WebMvcConfig implements WebMvcConfigurer {
+@Profile("prod")
+public class ProdCorsConfig implements WebMvcConfigurer {
+
+    private static final String[] ALLOWED_METHODS = {
+            "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
+    };
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .exposedHeaders(HttpHeaders.LOCATION);
+                .allowedOrigins("https://routie.me")
+                .allowedMethods(ALLOWED_METHODS)
+                .allowCredentials(true)
+                .allowedHeaders("*");
     }
 }
