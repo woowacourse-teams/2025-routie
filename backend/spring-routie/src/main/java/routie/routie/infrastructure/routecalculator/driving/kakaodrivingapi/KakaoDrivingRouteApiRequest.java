d3a34084b343a7ed18cce41ea25a387d8f6b6045
KimDaehyeon <daehyeon3351@gmail.com>
2025-08-08 10:18:01 +0900

    feat: 카카오 경로 api 요청 시 실제 데이터를 사용하도록 변경

--- backend/spring-routie/src/main/java/routie/routie/infrastructure/routecalculator/driving/kakaodrivingapi/KakaoDrivingRouteApiRequest.java
+++ backend/spring-routie/src/main/java/routie/routie/infrastructure/routecalculator/driving/kakaodrivingapi/KakaoDrivingRouteApiRequest.java
@@ -1,10 +1,10 @@
 package routie.routie.infrastructure.routecalculator.driving.kakaodrivingapi;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import routie.routie.domain.RoutiePlace;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
+import routie.place.domain.Place;
+import routie.routie.domain.RoutiePlace;
 
 public record KakaoDrivingRouteApiRequest(
         @JsonProperty("origin") CoordinateRequest origin,
@@ -12,29 +12,29 @@ public record KakaoDrivingRouteApiRequest(
         @JsonProperty("waypoints") List<CoordinateRequest> waypoints
 ) {
 
-    // TODO: 실제 API 호출 시에는 이 부분을 제거하고, 좌표를 사용해야 함.
-    private static final List<CoordinateRequest> MOCK_COORDINATE_RESPONSES = List.of(
-            new CoordinateRequest(127.0508759, 37.5059523),
-            new CoordinateRequest(127.0506698, 37.5057434),
-            new CoordinateRequest(127.1494305, 37.4385195)
-    );
 
     public static KakaoDrivingRouteApiRequest from(final List<RoutiePlace> routiePlaces) {
-        int size = routiePlaces.size();
-        List<CoordinateRequest> coordinateRequests = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            coordinateRequests.add(MOCK_COORDINATE_RESPONSES.get(i % MOCK_COORDINATE_RESPONSES.size()));
-        }
         return new KakaoDrivingRouteApiRequest(
-                coordinateRequests.getFirst(),
-                coordinateRequests.getLast(),
-                coordinateRequests.subList(1, size - 1)
+                CoordinateRequest.from(routiePlaces.getFirst()),
+                CoordinateRequest.from(routiePlaces.getLast()),
+                IntStream.range(1, routiePlaces.size() - 1)
+                        .mapToObj(routiePlaces::get)
+                        .map(CoordinateRequest::from)
+                        .toList()
         );
     }
 
-    private record CoordinateRequest(
+    public record CoordinateRequest(
             @JsonProperty("x") double longitude,
             @JsonProperty("y") double latitude
     ) {
+
+        public static CoordinateRequest from(final RoutiePlace routiePlace) {
+            Place place = routiePlace.getPlace();
+            return new CoordinateRequest(
+                    place.getLongitude(),
+                    place.getLatitude()
+            );
+        }
     }
 }
