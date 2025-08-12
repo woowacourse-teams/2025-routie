b80bb70adc98481c98b75ef96bcfa19706bf03e2
KimDaehyeon <daehyeon3351@gmail.com>
2025-08-12 20:22:03 +0900

    refactor: 검증 로직 메서드 분리

--- backend/spring-routie/src/main/java/routie/routie/domain/routievalidator/ValidationContext.java
+++ backend/spring-routie/src/main/java/routie/routie/domain/routievalidator/ValidationContext.java
@@ -10,14 +10,26 @@ public record ValidationContext(
 ) {
 
     public ValidationContext {
+        validateStartDateTime(startDateTime);
+        validateEndDateTime(endDateTime);
+        validateTimePeriods(timePeriods);
+    }
+
+    private void validateStartDateTime(final LocalDateTime startDateTime) {
         if (startDateTime == null) {
             throw new IllegalArgumentException("시작 시간은 null일 수 없습니다.");
         }
+    }
+
+    private void validateEndDateTime(final LocalDateTime endDateTime) {
         if (endDateTime == null) {
             throw new IllegalArgumentException("종료 시간은 null일 수 없습니다.");
         }
+    }
+
+    private void validateTimePeriods(final TimePeriods timePeriods) {
         if (timePeriods == null) {
-            throw new IllegalArgumentException("시간 기간은 null일 수 없습니다.");
+            throw new IllegalArgumentException("기간은 null일 수 없습니다.");
         }
     }
 }
