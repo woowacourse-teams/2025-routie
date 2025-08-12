d7ccd1b56c0b4a2eb8030143853f71aae2afcb20
youjeong Jeong <wjddbwjd1205@naver.com>
2025-08-12 18:18:06 +0900

    chore: 프로덕션, 개발 환경 분리를 위한 웹팩 세팅

---
+++ frontend/webpack.prod.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(common(), {
+  mode: 'production',
+  devtool: 'hidden-source-map',
+  plugins: [
+    new Dotenv({
+      path: './.env.prod',
+    }),
+  ],
+  optimization: {
+    splitChunks: { chunks: 'all' },
+  },
+});
