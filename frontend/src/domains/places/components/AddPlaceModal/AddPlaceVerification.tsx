f449e2a752ee577173bc7e44c5bfece5716793df
aydenote <dslgpgh@gmail.com>
2025-08-13 16:48:42 +0900

    refactor: 장소 추가 모달에서 관심사별 컴포넌트 분리

---
+++ frontend/src/domains/places/components/AddPlaceModal/AddPlaceVerification.tsx
@@ -0,0 +1,50 @@
+import Flex from '@/@common/components/Flex/Flex';
+
+import BreakTimeInputs from '../PlaceFormSection/BreakTimeInputs';
+import BusinessHourInputs from '../PlaceFormSection/BusinessHourInputs';
+import ClosedDaySelector from '../PlaceFormSection/ClosedDaySelector';
+import { FormState } from '../PlaceFormSection/PlaceForm.types';
+
+interface AddPlaceVerificationProps {
+  form: FormState;
+  isEmpty: Record<string, boolean>;
+  showErrors: boolean;
+  handleInputChange: (
+    field: keyof Omit<FormState, 'closedDayOfWeeks'>,
+    value: string,
+  ) => void;
+  handleToggleDay: (index: number) => void;
+}
+
+const AddPlaceVerification = ({
+  form,
+  isEmpty,
+  showErrors,
+  handleInputChange,
+  handleToggleDay,
+}: AddPlaceVerificationProps) => {
+  return (
+    <Flex direction="column" alignItems="flex-start" width="100%" gap={2}>
+      <BusinessHourInputs
+        openAt={form.openAt}
+        closeAt={form.closeAt}
+        onChange={handleInputChange}
+        error={{
+          openAt: showErrors && isEmpty.openAt,
+          closeAt: showErrors && isEmpty.closeAt,
+        }}
+      />
+      <BreakTimeInputs
+        breakStartAt={form.breakStartAt}
+        breakEndAt={form.breakEndAt}
+        onChange={handleInputChange}
+      />
+      <ClosedDaySelector
+        closedDayOfWeeks={form.closedDayOfWeeks}
+        onToggleDay={handleToggleDay}
+      />
+    </Flex>
+  );
+};
+
+export default AddPlaceVerification;
