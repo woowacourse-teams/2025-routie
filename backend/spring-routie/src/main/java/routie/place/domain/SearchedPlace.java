829bb80fe74ffbe4e3d87fa55c724b8122c11801
KimDaehyeon <daehyeon3351@gmail.com>
2025-08-13 21:57:17 +0900

    refactor: 지번 주소를 필수로, 도로명 주소를 nullable로 변경

--- backend/spring-routie/src/main/java/routie/place/domain/SearchedPlace.java
+++ backend/spring-routie/src/main/java/routie/place/domain/SearchedPlace.java
@@ -12,38 +12,38 @@ public record SearchedPlace(
     public SearchedPlace {
         validateSearchedPlaceId(searchedPlaceId);
         validateName(name);
-        validateRoadAddressName(roadAddressName);
+        validateAddressName(addressName);
         validateLongitude(longitude);
         validateLatitude(latitude);
     }
 
     private void validateSearchedPlaceId(final String searchedPlaceId) {
         if (searchedPlaceId == null || searchedPlaceId.isBlank()) {
-            throw new IllegalArgumentException("검색된 장소 ID는 필수 입력 사항입니다.");
+            throw new IllegalArgumentException("검색된 장소 ID는 필수값입니다.");
         }
     }
 
     private void validateName(final String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("장소 이름은 필수 입력 사항입니다.");
+            throw new IllegalArgumentException("장소 이름은 필수값입니다.");
         }
     }
 
-    private void validateRoadAddressName(final String roadAddressName) {
-        if (roadAddressName == null || roadAddressName.isBlank()) {
-            throw new IllegalArgumentException("도로명 주소는 필수 입력 사항입니다.");
+    private void validateAddressName(final String addressName) {
+        if (addressName == null || addressName.isBlank()) {
+            throw new IllegalArgumentException("지번은 필수값입니다.");
         }
     }
 
     private void validateLongitude(final double longitude) {
         if (longitude < -180 || longitude > 180) {
-            throw new IllegalArgumentException("유효한 경도 값을 입력해야 합니다.");
+            throw new IllegalArgumentException("유효한 경도 값이 아닙니다.");
         }
     }
 
     private void validateLatitude(final double latitude) {
         if (latitude < -90 || latitude > 90) {
-            throw new IllegalArgumentException("유효한 위도 값을 입력해야 합니다.");
+            throw new IllegalArgumentException("유효한 위도 값이 아닙니다.");
         }
     }
 }
