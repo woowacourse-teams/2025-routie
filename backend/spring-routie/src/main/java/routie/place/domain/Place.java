cda60f91f0336dd6b57711d2473dc2495e904783
threepebbles <threepebbles@naver.com>
2025-08-13 15:49:05 +0900

    refactor: indent 2 -> 1

--- backend/spring-routie/src/main/java/routie/place/domain/Place.java
+++ backend/spring-routie/src/main/java/routie/place/domain/Place.java
@@ -199,10 +199,12 @@ public class Place {
     }
 
     private void validateRoadAddressName(final String roadAddressName) {
-        if (roadAddressName != null) {
-            if (roadAddressName.isBlank() || roadAddressName.length() > 50) {
-                throw new IllegalArgumentException("도로명 주소는 1자 이상 50자 이하여야 합니다.");
-            }
+        if (roadAddressName == null) {
+            return;
+        }
+
+        if (roadAddressName.isBlank() || roadAddressName.length() > 50) {
+            throw new IllegalArgumentException("도로명 주소는 1자 이상 50자 이하여야 합니다.");
         }
     }
 
