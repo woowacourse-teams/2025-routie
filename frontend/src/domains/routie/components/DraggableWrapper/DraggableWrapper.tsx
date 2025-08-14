b6424479e1474b424b9fdbc663d3d419972a1ed4
youjeong Jeong <wjddbwjd1205@naver.com>
2025-08-14 16:54:36 +0900

    refactor: DraggableWrapper 스타일 분리

--- frontend/src/domains/routie/components/DraggableWrapper/DraggableWrapper.tsx
+++ frontend/src/domains/routie/components/DraggableWrapper/DraggableWrapper.tsx
@@ -1,4 +1,4 @@
-import { css } from '@emotion/react';
+import { DraggableWrapperStyle } from './DraggableWrapper.styles';
 interface DraggableCardWrapperProps
   extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -9,14 +9,3 @@ const DraggableWrapper = ({ children }: DraggableCardWrapperProps) => {
 };
 
 export default DraggableWrapper;
-
-const DraggableWrapperStyle = css`
-  cursor: grab;
-  border-radius: 1.2rem;
-  transition: transform 120ms ease, box-shadow 120ms ease, opacity 120ms ease;
-
-  &:hover {
-    transform: translateY(-0.2rem);
-    box-shadow: 0 0.8rem 2.4rem rgb(0 0 0 / 12%);
-  }
-`;
