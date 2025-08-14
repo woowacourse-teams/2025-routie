ed141fb13da5f37b9347e39d88757c529c504682
AHHYUNJU <wndkgus0225@naver.com>
2025-08-14 13:24:07 +0900

    feat: KakaoMap과 SideSheet를 결합한 MapWithSideSheet 컴포넌트 추가 및 적용

---
+++ frontend/src/layouts/MapWithSideSheet/MapWithSideSheet.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import KakaoMap from '@/domains/maps/components/KakaoMap';
+import Flex from '@/@common/components/Flex/Flex';
+import SideSheet from '../SideSheet/SideSheet';
+import { containerCss } from './MapWithSideSheet.styles';
+import Text from '@/@common/components/Text/Text';
+
+const MapWithSideSheet = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Flex>
+      <div css={containerCss}>
+        <KakaoMap />
+        <SideSheet open={open} onToggle={() => setOpen((v) => !v)}></SideSheet>
+      </div>
+    </Flex>
+  );
+};
+
+export default MapWithSideSheet;
--- frontend/src/pages/RoutieSpace/RoutieSpace.tsx
+++ frontend/src/pages/RoutieSpace/RoutieSpace.tsx
@@ -3,8 +3,10 @@ import useMapView from '@/@common/hooks/useMapView';
 import KakaoMap from '@/domains/maps/components/KakaoMap';
 import { RoutieProvider } from '@/domains/routie/contexts/useRoutieContext';
 import { RoutieValidateProvider } from '@/domains/routie/contexts/useRoutieValidateContext';
+import MapWithSideSheet from '@/layouts/MapWithSideSheet/MapWithSideSheet';
 import PlaceList from '@/layouts/PlaceList/PlaceList';
 import { PlaceListProvider } from '@/layouts/PlaceList/contexts/PlaceListProvider';
+import SideSheet from '@/layouts/SideSheet/SideSheet';
 import Sidebar from '@/layouts/Sidebar/Sidebar';
 
 const RoutieSpace = () => {
@@ -20,7 +22,7 @@ const RoutieSpace = () => {
             </Flex>
             <Flex direction="column" justifyContent="flex-start" height="100%">
               {viewMode === 'list' && <PlaceList />}
-              {viewMode === 'map' && <KakaoMap />}
+              {viewMode === 'map' && <MapWithSideSheet />}
             </Flex>
           </Flex>
         </PlaceListProvider>
