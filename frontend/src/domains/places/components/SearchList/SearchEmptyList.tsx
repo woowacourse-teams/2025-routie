290f2e92357d78b819a90836b76180719a25849f
aydenote <dslgpgh@gmail.com>
2025-08-14 20:46:46 +0900

    feat: 검색 결과가 없을 때 나오는 컴포넌트 구현

---
+++ frontend/src/domains/places/components/SearchList/SearchEmptyList.tsx
@@ -0,0 +1,28 @@
+import Text from '@/@common/components/Text/Text';
+
+import { itemButtonStyle, listStyle } from '../SearchList/SearchList.styles';
+
+interface SearchEmptyList {
+  keyword: string;
+}
+
+const SearchEmptyList = ({ keyword }: SearchEmptyList) => {
+  return (
+    <ul css={listStyle}>
+      <li
+        css={[
+          itemButtonStyle,
+          {
+            cursor: 'default',
+            ':hover': { background: 'none' },
+          },
+        ]}
+      >
+        <Text variant="label">
+          {`"${keyword}"에 대한 검색 결과가 없습니다.`}
+        </Text>
+      </li>
+    </ul>
+  );
+};
+export default SearchEmptyList;
