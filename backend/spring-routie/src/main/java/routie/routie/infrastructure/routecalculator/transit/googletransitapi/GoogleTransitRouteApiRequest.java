989ef348e40ce3705d3c6484f0b07aee0432c0ca
goohong <goohong.chung@gmail.com>
2025-08-16 11:47:03 +0900

    refactor: GoogleTransitRouteApiRequest를 Record에서 클래스로 변환하여 생성자 제한

--- backend/spring-routie/src/main/java/routie/routie/infrastructure/routecalculator/transit/googletransitapi/GoogleTransitRouteApiRequest.java
+++ backend/spring-routie/src/main/java/routie/routie/infrastructure/routecalculator/transit/googletransitapi/GoogleTransitRouteApiRequest.java
@@ -4,17 +4,43 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import routie.place.domain.Place;
 import routie.routie.domain.RoutiePlace;
 
-public record GoogleTransitRouteApiRequest(
-        @JsonProperty("origin") CoordinateRequest origin,
-        @JsonProperty("destination") CoordinateRequest destination,
-        @JsonProperty("travelMode") String travelMode
-) {
+public final class GoogleTransitRouteApiRequest {
+    @JsonProperty("origin")
+    private final CoordinateRequest origin;
+    @JsonProperty("destination")
+    private final CoordinateRequest destination;
+    @JsonProperty("travelMode")
+    private final String travelMode;
+
+    public GoogleTransitRouteApiRequest(
+            @JsonProperty("origin") final CoordinateRequest origin,
+            @JsonProperty("destination") final CoordinateRequest destination
+    ) {
+        this.origin = origin;
+        this.destination = destination;
+        this.travelMode = "TRANSIT";
+    }
+
     public static GoogleTransitRouteApiRequest from(final RoutiePlace from, final RoutiePlace to) {
         return new GoogleTransitRouteApiRequest(
                 CoordinateRequest.from(from),
-                CoordinateRequest.from(to),
-                "TRANSIT"
+                CoordinateRequest.from(to)
         );
+    }
+
+    @JsonProperty("origin")
+    public CoordinateRequest origin() {
+        return origin;
+    }
+
+    @JsonProperty("destination")
+    public CoordinateRequest destination() {
+        return destination;
+    }
+
+    @JsonProperty("travelMode")
+    public String travelMode() {
+        return travelMode;
     }
 
     public record CoordinateRequest(
