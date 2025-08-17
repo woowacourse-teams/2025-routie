476ceaaaa8709647c35bc96c79e9239347dba40f
aydenote <dslgpgh@gmail.com>
2025-08-17 19:56:06 +0900

    feat: 입력에 대한 안내 메시지 추가

--- frontend/src/domains/places/components/PlaceFormSection/BusinessHourInputs.tsx
+++ frontend/src/domains/places/components/PlaceFormSection/BusinessHourInputs.tsx
@@ -20,28 +20,33 @@ const BusinessHourInputs = ({
 }: BusinessHourInputsProps) => {
   return (
     <>
+      <Text variant="label">
+        세밀한 동선 검증을 원하신다면, 실제 영업 시간과 영업일을 입력해주세요!
+      </Text>
       <Text variant="caption">영업시간</Text>
-      <Flex justifyContent="space-between" width="100%" gap={1}>
-        <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
-          <Input
-            id="openAt"
-            type="time"
-            value={openAt}
-            onChange={(value) => onChange('openAt', value)}
-            variant={error?.openAt ? 'error' : 'primary'}
-            label="오픈 시간"
-            autoFocus
-          />
-        </Flex>
-        <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
-          <Input
-            id="closeAt"
-            type="time"
-            value={closeAt}
-            onChange={(value) => onChange('closeAt', value)}
-            variant={error?.closeAt ? 'error' : 'primary'}
-            label="마감 시간"
-          />
+      <Flex direction="column" gap={0.5} width="100%" alignItems="flex-start">
+        <Flex justifyContent="space-between" width="100%" gap={1}>
+          <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
+            <Input
+              id="openAt"
+              type="time"
+              value={openAt}
+              onChange={(value) => onChange('openAt', value)}
+              variant={error?.openAt ? 'error' : 'primary'}
+              label="오픈 시간"
+              autoFocus
+            />
+          </Flex>
+          <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
+            <Input
+              id="closeAt"
+              type="time"
+              value={closeAt}
+              onChange={(value) => onChange('closeAt', value)}
+              variant={error?.closeAt ? 'error' : 'primary'}
+              label="마감 시간"
+            />
+          </Flex>
         </Flex>
       </Flex>
     </>
