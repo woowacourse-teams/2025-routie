55c492ce35d46b129625cd49a41fd16e6290f434
aydenote <dslgpgh@gmail.com>
2025-08-17 22:16:29 +0900

    feat: 버튼을 통해 체류 시간 증감 기능 구현

--- frontend/src/domains/places/components/PlaceFormSection/StayDurationInput.tsx
+++ frontend/src/domains/places/components/PlaceFormSection/StayDurationInput.tsx
@@ -1,5 +1,10 @@
+import { css } from '@emotion/react';
+
+import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
 import Input from '@/@common/components/Input/Input';
+import Text from '@/@common/components/Text/Text';
+import theme from '@/styles/theme';
 
 interface StayDurationInputProps {
   value: number;
@@ -13,18 +18,57 @@ const StayDurationInput = ({
   error = false,
 }: StayDurationInputProps) => {
   const inputVariant = error ? 'error' : 'primary';
+  const stayDurationStepOptions = [-30, -10, 10, 30];
+
+  const handleIncreaseButton = (buttonNumber: number) => {
+    onChange('stayDurationMinutes', String(Math.max(0, value + buttonNumber)));
+  };
 
   return (
-    <Flex direction="column" alignItems="flex-start" gap={1} width="100%">
-      <Input
-        id="stayDurationMinutes"
-        type="number"
-        value={value === -1 ? '' : value.toString()}
-        onChange={(value) => onChange('stayDurationMinutes', value)}
-        variant={inputVariant}
-        label="체류 시간 (분)"
-        placeholder="체류 시간을 입력해주세요"
-      />
+    <Flex direction="column" alignItems="flex-start" gap={0.75} width="100%">
+      <Text variant="caption">체류 시간 (분)</Text>
+
+      <Flex
+        gap={0.6}
+        width="100%"
+        css={css`
+          flex: 0 0 auto;
+        `}
+      >
+        <div
+          css={css`
+            flex: 0 0 auto;
+          `}
+        >
+          <Input
+            id="stayDurationMinutes"
+            type="number"
+            value={value.toString()}
+            onChange={(v) => onChange('stayDurationMinutes', v)}
+            variant={inputVariant}
+            placeholder="체류 시간을 입력해주세요"
+            step={10}
+            min={0}
+          />
+        </div>
+
+        {stayDurationStepOptions.map((durationStep) => (
+          <Button
+            key={durationStep}
+            onClick={() => handleIncreaseButton(durationStep)}
+            css={css`
+              justify-content: center;
+
+              :hover {
+                color: ${theme.colors.white};
+                background-color: ${theme.colors.purple[300]};
+              }
+            `}
+          >
+            {durationStep > 0 ? `+${durationStep}` : durationStep}
+          </Button>
+        ))}
+      </Flex>
     </Flex>
   );
 };
