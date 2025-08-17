8ad1161cee8cef3073535b75587873dc3722f6c3
Seungsoo Choi <77476077+aydenote@users.noreply.github.com>
2025-08-17 12:55:58 +0900

    feat: 검색한 장소에 대해 결과가 있다면 도로명 또는 지번 사용하도록 구현 (#531)

--- frontend/src/domains/places/components/SearchBox/SearchBox.tsx
+++ frontend/src/domains/places/components/SearchBox/SearchBox.tsx
@@ -1,3 +1,5 @@
+import { css } from '@emotion/react';
+
 import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
 import Input from '@/@common/components/Input/Input';
@@ -6,6 +8,7 @@ import theme from '@/styles/theme';
 
 import useSearchPlace from '../../hooks/useSearchPlace';
 import { PlaceLocationType, PlaceSearchType } from '../../types/place.types';
+import SearchEmptyState from '../SearchList/SearchEmptyState';
 import SearchList from '../SearchList/SearchList';
 
 interface SearchBoxProps {
@@ -25,6 +28,8 @@ const SearchBox = ({ onChange, handleSearchPlaceMap }: SearchBoxProps) => {
     handleReset,
     handleEnterSearch,
   } = useSearchPlace();
+  const hasResults = searchResults && searchResults.length > 0;
+  const isEmpty = searchResults && searchResults.length === 0;
 
   const handleSelect = (searchPlace: PlaceSearchType) => {
     handleReset();
@@ -40,42 +45,53 @@ const SearchBox = ({ onChange, handleSearchPlaceMap }: SearchBoxProps) => {
   };
 
   return (
-    <>
-      <Flex direction="row" gap={1} width="100%" css={{ position: 'relative' }}>
-        <Input
-          id="search"
-          value={keyword}
-          icon="search"
-          placeholder="장소를 검색하세요"
-          onChange={handleChangeKeyword}
-          onKeyDown={handleEnterSearch}
-          autoFocus
-        />
-        <Button
-          variant="primary"
-          width="20%"
-          type="button"
-          onClick={handleSearch}
-        >
-          <Flex width="100%">
-            <Text color={theme.colors.white} variant="label">
-              검색
-            </Text>
-          </Flex>
-        </Button>
-        <Flex
-          width="100%"
-          css={{ position: 'absolute', top: '100%', marginTop: '1rem' }}
-        >
-          {searchResults.length > 0 && (
-            <SearchList
-              searchResults={searchResults}
-              handleSelect={handleSelect}
-            />
-          )}
+    <Flex
+      gap={1}
+      width="100%"
+      css={css`
+        position: relative;
+      `}
+    >
+      <Input
+        id="search"
+        value={keyword}
+        icon="search"
+        placeholder="장소를 검색하세요"
+        onChange={handleChangeKeyword}
+        onKeyDown={handleEnterSearch}
+        autoFocus
+      />
+      <Button
+        variant="primary"
+        width="20%"
+        type="button"
+        onClick={handleSearch}
+        disabled={keyword ? false : true}
+      >
+        <Flex width="100%">
+          <Text color={theme.colors.white} variant="label">
+            검색
+          </Text>
         </Flex>
+      </Button>
+      <Flex
+        width="100%"
+        css={css`
+          position: absolute;
+          top: 100%;
+          margin-top: 1rem;
+        `}
+      >
+        {hasResults ? (
+          <SearchList
+            searchResults={searchResults!}
+            handleSelect={handleSelect}
+          />
+        ) : (
+          isEmpty && <SearchEmptyState keyword={keyword} />
+        )}
       </Flex>
-    </>
+    </Flex>
   );
 };
 
