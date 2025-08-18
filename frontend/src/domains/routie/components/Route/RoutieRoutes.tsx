0444c3df9050bbb24031c1d2ccb5af079307f54f
ohgus <anakin1108@naver.com>
2025-08-18 21:39:51 +0900

    refactor: 유저가 선택한 이동 수단이 UI에 반영될 수 있게 수정

--- frontend/src/domains/routie/components/Route/RoutieRoutes.tsx
+++ frontend/src/domains/routie/components/Route/RoutieRoutes.tsx
@@ -4,7 +4,7 @@ import Text from '@/@common/components/Text/Text';
 import theme from '@/styles/theme';
 
 import { MOVING_EN_TO_KR } from '../../constants/translate';
-import { Routes, Routie } from '../../types/routie.types';
+import { useRoutieContext } from '../../contexts/useRoutieContext';
 import { convertMetersToKilometers } from '../../utils/format';
 import formatMinutesToHours from '../../utils/formatMinutesToHours';
 
@@ -14,10 +14,13 @@ interface RoutieRoutesProps {
 }
 
 const RoutieRoutes = ({ routie, routes }: RoutieRoutesProps) => {
+  const { movingStrategy } = useRoutieContext();
+
   return (
     <Flex key={routie.id} margin={1} gap={1}>
       <Text variant="description">
-        {MOVING_EN_TO_KR[routes.movingStrategy]} {formatMinutesToHours(routes.duration)}
+        {MOVING_EN_TO_KR[movingStrategy]}{' '}
+        {formatMinutesToHours(routes.duration)}
       </Text>
       <Pill type="distance">
         <Text variant="description" color={theme.colors.purple[400]}>
