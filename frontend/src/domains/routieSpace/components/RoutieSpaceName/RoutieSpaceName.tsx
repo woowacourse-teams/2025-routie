2b6b86e9d021ab9445524465a1ac7a73d82a30ed
ohgus <anakin1108@naver.com>
2025-08-20 00:20:50 +0900

    refactor: useRoutieSpace 훅 구현

--- frontend/src/domains/routieSpace/components/RoutieSpaceName/RoutieSpaceName.tsx
+++ frontend/src/domains/routieSpace/components/RoutieSpaceName/RoutieSpaceName.tsx
@@ -1,46 +1,23 @@
-import { useEffect, useState } from 'react';
-
+import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
-import IconButton from '@/@common/components/IconButton/IconButton';
 import Text from '@/@common/components/Text/Text';
-import editIcon from '@/assets/icons/edit.svg';
+import theme from '@/styles/theme';
 
-import {
-  editRoutieSpaceName,
-  getRoutieSpaceName,
-} from '../../apis/routieSpaceName';
+import useRoutieSpaceName from '../../hooks/useRoutieSpaceName';
 
 import routieSpaceNameInputStyle from './RoutieSpaceName.style';
 
 const RoutieSpaceName = () => {
-  const [name, setName] = useState('');
-  const [isEditing, setIsEditing] = useState(false);
-
-  useEffect(() => {
-    const fetchRoutieSpaceName = async () => {
-      const name = await getRoutieSpaceName();
-      setName(name ?? '이름 못 찾음');
-    };
-    fetchRoutieSpaceName();
-  }, []);
-
-  const handleClick = async () => {
-    if (isEditing) {
-      try {
-        const updatedName = await editRoutieSpaceName(name);
-        setName(updatedName ?? '이름 못 찾음');
-      } catch (error) {
-        console.error('루티 스페이스 이름 수정 중 에러 발생:', error);
-      }
-      setIsEditing(false);
-    } else {
-      setIsEditing(true);
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
+  const {
+    name,
+    isEditing,
+    isLoading,
+    errorCase,
+    inputRef,
+    handleEnter,
+    handleClick,
+    handleChange,
+  } = useRoutieSpaceName();
 
   return (
     <Flex
@@ -52,24 +29,25 @@ const RoutieSpaceName = () => {
     >
       {isEditing ? (
         <input
-          id="routie-space-name"
-          css={routieSpaceNameInputStyle}
+          ref={inputRef}
+          css={routieSpaceNameInputStyle(errorCase === 'invalidNameLength')}
           maxLength={20}
           autoFocus
           value={name}
           onChange={handleChange}
+          onKeyDown={handleEnter}
         />
       ) : (
-        <Flex
-          alignItems="center"
-          justifyContent="flex-start"
-          padding={0.4}
-          width="100%"
-        >
+        <Flex justifyContent="flex-start" padding={0.4} width="100%">
           <Text variant="title">{name}</Text>
         </Flex>
       )}
-      <Button variant="primary" onClick={handleClick} width="6.5rem">
+      <Button
+        variant="primary"
+        onClick={handleClick}
+        width="7rem"
+        disabled={isLoading}
+      >
         <Flex width="100%">
           <Text variant="subTitle" color={theme.colors.white}>
             {isEditing ? '저장' : '수정'}
