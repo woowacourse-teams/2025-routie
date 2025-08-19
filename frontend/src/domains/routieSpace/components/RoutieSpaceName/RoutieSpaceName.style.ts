a93f870ce210fafa6a00fecb488bf4dea1ad1988
ohgus <anakin1108@naver.com>
2025-08-20 00:19:41 +0900

    feat: error 스타일 추가

--- frontend/src/domains/routieSpace/components/RoutieSpaceName/RoutieSpaceName.style.ts
+++ frontend/src/domains/routieSpace/components/RoutieSpaceName/RoutieSpaceName.style.ts
@@ -2,19 +2,30 @@ import { css } from '@emotion/react';
 
 import theme from '@/styles/theme';
 
-const routieSpaceNameInputStyle = css`
+const defaultBorder = css`
+  &:focus {
+    outline: 0.3rem solid ${theme.colors.purple[300]};
+  }
+`;
+
+const invalidBorder = css`
+  &:focus {
+    outline: 0.3rem solid ${theme.colors.red[100]};
+  }
+`;
+
+const routieSpaceNameInputStyle = (invalidNameLength: boolean) => css`
   width: 100%;
+  max-width: 38rem;
   margin: 0;
   padding: 0.4rem;
   border: none;
+  border-radius: 8px;
 
   font-size: ${theme.font.size.heading};
   font-weight: ${theme.font.weight.bold};
 
-  &:focus {
-    border-radius: 8px;
-    outline: 0.3rem solid ${theme.colors.purple[300]};
-  }
+  ${invalidNameLength ? invalidBorder : defaultBorder}
 `;
 
 export default routieSpaceNameInputStyle;
