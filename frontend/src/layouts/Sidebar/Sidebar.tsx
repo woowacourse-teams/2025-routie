0dab4617555a05994dd4f58e2328c2e5a018b1c1
ohgus <anakin1108@naver.com>
2025-08-20 20:34:06 +0900

    refactor: UI 재배치 및 스타일 파일 분리

--- frontend/src/layouts/Sidebar/Sidebar.tsx
+++ frontend/src/layouts/Sidebar/Sidebar.tsx
@@ -12,9 +12,13 @@ import SelectMovingStrategy from '@/domains/routie/components/SelectMovingStrate
 import { useRoutieContext } from '@/domains/routie/contexts/useRoutieContext';
 import { useRoutieValidateContext } from '@/domains/routie/contexts/useRoutieValidateContext';
 import RoutieSpaceName from '@/domains/routieSpace/components/RoutieSpaceName/RoutieSpaceName';
-import theme from '@/styles/theme';
 
 import DateInput from './DateInput';
+import {
+  SidebarSectionStyle,
+  SidebarContainerStyle,
+  RoutieSectionScrollStyle,
+} from './Sidebar.styles';
 import TimeInput from './TimeInput';
 
 const Sidebar = () => {
@@ -50,73 +54,74 @@ const Sidebar = () => {
       <Flex
         direction="column"
         justifyContent="flex-start"
-        alignItems="flex-start"
         width="50rem"
-        height="100vh"
-        gap={1}
-        style={{
-          overflow: 'hidden',
-          borderRight: `1px solid ${theme.colors.black}`,
-        }}
+        height="100dvh"
+        gap={1.6}
+        css={SidebarContainerStyle}
       >
         <Header isHome={false}>
           <RoutieSpaceName />
         </Header>
         <Flex
           direction="column"
-          width="100%"
-          gap={1.2}
+          gap={2}
           padding={1.6}
+          width="90%"
           justifyContent="flex-start"
+          css={SidebarSectionStyle(false)}
         >
           <Flex width="100%" justifyContent="flex-start" gap={1}>
             <Text variant="title2">언제, 어떻게 떠날까요?</Text>
           </Flex>
-          <Flex justifyContent="flex-end" width="100%" gap={1}>
-            <Text variant="subTitle">일정 검증</Text>
-            <ToggleSwitch
-              active={isValidateActive}
-              onToggle={handleValidateToggle}
-            />
+          <Flex width="100%" gap={1.2}>
+            <DateInput />
+            <TimeInput />
           </Flex>
-          {renderValidationCard()}
           <SelectMovingStrategy />
-          <DateInput />
-          <TimeInput />
         </Flex>
         <Flex
           direction="column"
           alignItems="flex-start"
-          width="100%"
+          justifyContent="flex-start"
+          width="90%"
+          height="100%"
           gap={1.2}
           padding={1.6}
-          style={{
-            overflow: 'hidden',
-          }}
+          css={SidebarSectionStyle(true)}
         >
           <Flex padding={0.5} justifyContent="space-between" width="100%">
-            <Text variant="subTitle">내 동선</Text>
-            <Text variant="caption" color="gray">
-              {routiePlaces.length}개의 장소가 동선에 추가되었어요
-            </Text>
+            <Flex width="100%" justifyContent="flex-start" gap={1}>
+              <Text variant="title2">내가 갈 곳</Text>
+              <Text variant="label" color="gray">
+                {routiePlaces.length}개의 장소
+              </Text>
+            </Flex>
+            <Flex width="100%" justifyContent="flex-end" gap={1.5}>
+              <Text variant="subTitle">일정 검증</Text>
+              <ToggleSwitch
+                active={isValidateActive}
+                onToggle={handleValidateToggle}
+              />
+            </Flex>
           </Flex>
+          {renderValidationCard()}
+
           {routiePlaces.length === 0 && (
-            <EmptyMessage
-              messages={[
-                '아직 동선이 없습니다.',
-                '장소 목록에서 2곳 이상을 선택하면 동선이 생성됩니다!',
-              ]}
-            />
+            <Flex width="100%" height="100%">
+              <EmptyMessage
+                messages={[
+                  '아직 동선이 없습니다.',
+                  '장소 목록에서 2곳 이상을 선택하면 동선이 생성됩니다!',
+                ]}
+              />
+            </Flex>
           )}
           <Flex
             direction="column"
             justifyContent="flex-start"
             width="100%"
             padding={1.6}
-            style={{
-              overflowY: 'auto',
-              boxSizing: 'border-box',
-            }}
+            css={RoutieSectionScrollStyle}
           >
             <RoutieSection />
           </Flex>
