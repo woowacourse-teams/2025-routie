fa11ac2211f66520d04ff4c5ede3d28f951a7100
threepebbles <threepebbles@naver.com>
2025-08-20 14:22:54 +0900

    test: RoutieSpace에 대한 Builder, Fixture 추가 및 적용

--- backend/spring-routie/src/test/java/routie/routiespace/domain/RoutieSpaceFixture.java
+++ backend/spring-routie/src/test/java/routie/routiespace/domain/RoutieSpaceFixture.java
@@ -3,16 +3,34 @@ package routie.routiespace.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 import routie.place.domain.Place;
 import routie.routie.domain.Routie;
 
 public class RoutieSpaceFixture {
 
-    public static RoutieSpace createWithoutId(final List<Place> places, final Routie routie) {
-        return new RoutieSpace(null, "test-space", UUID.randomUUID().toString(), places, routie, null, null);
+    private static final AtomicLong sequence = new AtomicLong(0L);
+
+    public static String anyIdentifier() {
+        return UUID.randomUUID().toString();
     }
 
-    public static RoutieSpace createEmpty() {
-        return createWithoutId(new ArrayList<>(), Routie.empty());
+    public static String anyName() {
+        return "Defualt Routie Space Name" + sequence.incrementAndGet();
+    }
+
+    public static Routie emptyRoutie() {
+        return Routie.empty();
+    }
+
+    public static List<Place> emptyPlaces() {
+        return new ArrayList<>();
+    }
+
+    public static RoutieSpace emptyRoutieSpace() {
+        return new RoutieSpaceBuilder()
+                .places(emptyPlaces())
+                .routie(emptyRoutie())
+                .build();
     }
 }
