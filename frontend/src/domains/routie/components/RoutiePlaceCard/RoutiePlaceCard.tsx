986f660b711bd0d3c8b2bc838bdfb6003a95f1d4
aydenote <dslgpgh@gmail.com>
2025-08-20 15:28:30 +0900

    chore: 깃 충돌 해결

--- frontend/src/domains/routie/components/RoutiePlaceCard/RoutiePlaceCard.tsx
+++ frontend/src/domains/routie/components/RoutiePlaceCard/RoutiePlaceCard.tsx
@@ -126,7 +126,7 @@ const RoutiePlaceCard = ({ routie }: { routie: Routie }) => {
 
                 <Flex gap={0.4} alignItems="center">
                   <Icon name="pin" size={12} />
-                  <Text variant="label" color={theme.colors.gray[300]}>
+                  <Text variant="label" color={theme.colors.gray[300]} ellipsis>
                     {place.roadAddressName || place.addressName}
                   </Text>
                 </Flex>
