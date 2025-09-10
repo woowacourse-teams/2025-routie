79dbdb874047e08e4b22a6402449ac1cbb67fc4c
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-09-10 23:49:07 +0900

    refactor: export 형식 변경

--- frontend/src/layouts/Sidebar/Sidebar.styles.ts
+++ frontend/src/layouts/Sidebar/Sidebar.styles.ts
@@ -2,19 +2,21 @@ import { css } from '@emotion/react';
 
 import theme from '@/styles/theme';
 
-export const SidebarSectionStyle = (overflow: boolean) => css`
+const SidebarSectionStyle = (overflow: boolean) => css`
   overflow: ${overflow ? 'hidden' : ''};
   border-radius: 8px;
   background-color: ${theme.home.gray};
   box-shadow: 0 0 1rem 0 rgb(0 0 0 / 20%);
 `;
 
-export const SidebarContainerStyle = css`
+const SidebarContainerStyle = css`
   overflow: hidden;
   padding-bottom: 1.6rem;
   border-right: 1px solid ${theme.colors.black};
 `;
 
-export const RoutieSectionScrollStyle = css`
+const RoutieSectionScrollStyle = css`
   overflow-y: auto;
 `;
+
+export { SidebarSectionStyle, SidebarContainerStyle, RoutieSectionScrollStyle };
