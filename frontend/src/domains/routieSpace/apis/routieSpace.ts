b9e2c669cd5f928b85c86a62c8434928eabbbaad
ohgus <anakin1108@naver.com>
2025-09-10 17:57:37 +0900

    refactor: 흩어진 api 함수 한 파일로 병합

---
+++ frontend/src/domains/routieSpace/apis/routieSpace.ts
@@ -0,0 +1,53 @@
+import { apiClient } from '@/apis';
+
+import type {
+  CreateRoutieResponseType,
+  GetRoutieSpaceResponseType,
+  EditRoutieSpaceNameResponseType,
+} from '../types/routieSpace.types';
+
+const createRoutieSpace = async (): Promise<CreateRoutieResponseType> => {
+  const response = await apiClient.post('/routie-spaces');
+
+  const data = await response.json();
+
+  if (!data) {
+    throw new Error('루티 스페이스 생성에 실패했습니다.');
+  }
+
+  return data;
+};
+
+const getRoutieSpace = async (): Promise<GetRoutieSpaceResponseType> => {
+  const routieSpaceUuid = localStorage.getItem('routieSpaceUuid');
+
+  if (!routieSpaceUuid) {
+    throw new Error('루티 스페이스 uuid가 없습니다.');
+  }
+
+  const response = await apiClient.get(`/routie-spaces/${routieSpaceUuid}`);
+
+  const data = await response.json();
+
+  return data;
+};
+
+const editRoutieSpaceName = async (
+  name: string,
+): Promise<EditRoutieSpaceNameResponseType> => {
+  const routieSpaceUuid = localStorage.getItem('routieSpaceUuid');
+
+  if (!routieSpaceUuid) {
+    throw new Error('루티 스페이스 uuid가 없습니다.');
+  }
+
+  const response = await apiClient.patch(`/routie-spaces/${routieSpaceUuid}`, {
+    name,
+  });
+
+  const data = await response.json();
+
+  return data;
+};
+
+export { createRoutieSpace, getRoutieSpace, editRoutieSpaceName };
