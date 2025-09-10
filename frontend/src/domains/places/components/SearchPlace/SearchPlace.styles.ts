c827658481c0321aa36ad05e5164748b379a0975
AHHYUNJU <wndkgus0225@naver.com>
2025-09-10 19:59:26 +0900

    feat: SearchPlace 컴포넌트로 분리 및 조건부 렌더링 수정

---
+++ frontend/src/domains/places/components/SearchPlace/SearchPlace.styles.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+import theme from '@/styles/theme';
+
+const SearchNoticeStyle = css`
+  position: relative;
+  height: 100%;
+  color: ${theme.colors.black};
+`;
+
+export { SearchNoticeStyle };
