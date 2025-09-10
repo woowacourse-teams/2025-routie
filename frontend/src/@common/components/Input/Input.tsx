79b8ed6af668d738edd33becadd8aa01722249de
ohgus <anakin1108@naver.com>
2025-09-10 14:34:11 +0900

    refactor: Input 컴포넌트 컨벤션 수정

--- frontend/src/@common/components/Input/Input.tsx
+++ frontend/src/@common/components/Input/Input.tsx
@@ -8,7 +8,8 @@ import {
   InputIconStyle,
   InputLabelStyle,
 } from './Input.style';
-import { InputProps } from './Input.types';
+
+import type { InputProps } from './Input.types';
 
 const Input = ({
   id,
