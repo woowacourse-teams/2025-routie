5b8a8b3a10e94eead2c4e78da48e7c0bf94a80f3
ohgus <anakin1108@naver.com>
2025-09-10 17:17:14 +0900

    refactor: export 형식 변경

--- frontend/src/domains/routie/components/DraggableWrapper/DraggableWrapper.styles.ts
+++ frontend/src/domains/routie/components/DraggableWrapper/DraggableWrapper.styles.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 
-export const DraggableWrapperStyle = css`
+const DraggableWrapperStyle = css`
   cursor: grab;
   border-radius: 1.2rem;
   transition: transform 120ms ease, box-shadow 120ms ease, opacity 120ms ease;
@@ -10,3 +10,5 @@ export const DraggableWrapperStyle = css`
     box-shadow: 0 0.8rem 2.4rem rgb(0 0 0 / 12%);
   }
 `;
+
+export { DraggableWrapperStyle };
