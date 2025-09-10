4484600bfaa3e985772e55011e1e06b68b414102
AHHYUNJU <wndkgus0225@naver.com>
2025-09-10 19:49:06 +0900

    refactor: 검색 결과가 없을 때, EmptyMessage 컴포넌트를 활용하도록 수정

--- frontend/src/domains/places/components/SearchList/SearchEmptyState.tsx
+++ frontend/src/domains/places/components/SearchList/SearchEmptyState.tsx
@@ -1,6 +1,6 @@
-import Text from '@/@common/components/Text/Text';
+import EmptyMessage from '@/@common/components/EmptyMessage/EmptyMessage';
 
-import { itemEmptyStyle, listStyle } from './SearchList.styles';
+import { ListStyle } from './SearchList.styles';
 
 interface SearchEmptyStateProps {
   keyword: string;
@@ -8,12 +8,13 @@ interface SearchEmptyStateProps {
 
 const SearchEmptyState = ({ keyword }: SearchEmptyStateProps) => {
   return (
-    <ul css={listStyle}>
-      <li css={itemEmptyStyle}>
-        <Text variant="label">
-          {`"${keyword}"에 대한 검색 결과가 없습니다.`}
-        </Text>
-      </li>
+    <ul css={ListStyle}>
+      <EmptyMessage
+        messages={[
+          `"${keyword}"에 대한 검색 결과가 없습니다.`,
+          '장소를 다시 검색해주세요!',
+        ]}
+      />
     </ul>
   );
 };
