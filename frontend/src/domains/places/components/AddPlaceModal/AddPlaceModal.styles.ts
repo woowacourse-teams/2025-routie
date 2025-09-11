ffb8eb0b267d07dca4d2fa0c9c1287360151e77e
ohgus <anakin1108@naver.com>
2025-09-11 18:08:39 +0900

    refactor: 컨벤션 수정

--- frontend/src/domains/places/components/AddPlaceModal/AddPlaceModal.styles.ts
+++ frontend/src/domains/places/components/AddPlaceModal/AddPlaceModal.styles.ts
@@ -2,9 +2,11 @@ import { css } from '@emotion/react';
 
 import theme from '@/styles/theme';
 
-export const ModalInputContainerStyle = css`
+const ModalInputContainerStyle = css`
   width: 100%;
   padding: 1.6rem;
   border: 1px solid ${theme.colors.purple[200]};
   border-radius: 8px;
 `;
+
+export { ModalInputContainerStyle };
