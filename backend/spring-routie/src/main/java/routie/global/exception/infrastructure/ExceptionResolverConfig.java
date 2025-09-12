17837bf924993cfd95120d9ebdc6e843cc81e37d
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-13 02:19:03 +0900

    feat: ExceptionResolver 파사드 의존성 주입 및 Primary Bean 등록 Config 작성

---
+++ backend/spring-routie/src/main/java/routie/global/exception/infrastructure/ExceptionResolverConfig.java
@@ -0,0 +1,22 @@
+package routie.global.exception.infrastructure;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import routie.global.exception.domain.ExceptionResolver;
+import routie.global.exception.infrastructure.resolver.expected.ExpectedExceptionResolver;
+import routie.global.exception.infrastructure.resolver.unexpected.UnexpectedExceptionResolver;
+
+@Configuration
+public class ExceptionResolverConfig {
+
+    @Bean
+    @Primary
+    public ExceptionResolver exceptionResolverFacade(
+            final List<ExpectedExceptionResolver<? extends Exception>> expectedExceptionResolvers,
+            final UnexpectedExceptionResolver unexpectedExceptionResolver
+    ) {
+        return new ExceptionResolverFacade(expectedExceptionResolvers, unexpectedExceptionResolver);
+    }
+}
