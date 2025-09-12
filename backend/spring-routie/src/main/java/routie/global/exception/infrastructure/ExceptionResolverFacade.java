c2b88e7ded461e092e3a63b24cd216e96758be1f
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-13 02:18:39 +0900

    feat: 예상 가능한, 예상하지 못한 예외를 처리하는 복잡한 로직을 추상화하기 위한 ExceptionResolver 파사드 구현

---
+++ backend/spring-routie/src/main/java/routie/global/exception/infrastructure/ExceptionResolverFacade.java
@@ -0,0 +1,48 @@
+package routie.global.exception.infrastructure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import routie.global.exception.domain.ErrorCode;
+import routie.global.exception.domain.ExceptionDetail;
+import routie.global.exception.domain.ExceptionResolver;
+import routie.global.exception.infrastructure.resolver.expected.ExpectedExceptionResolver;
+import routie.global.exception.infrastructure.resolver.unexpected.UnexpectedExceptionResolver;
+
+@Slf4j
+public class ExceptionResolverFacade implements ExceptionResolver {
+
+    private final Map<Class<? extends Exception>, ExpectedExceptionResolver<? extends Exception>> expectedExceptionResolvers;
+    private final UnexpectedExceptionResolver unexpectedExceptionResolvers;
+
+    public ExceptionResolverFacade(
+            final List<ExpectedExceptionResolver<? extends Exception>> expectedExceptionResolvers,
+            final UnexpectedExceptionResolver unexpectedExceptionResolvers) {
+        this.expectedExceptionResolvers = expectedExceptionResolvers.stream()
+                .collect(Collectors.toMap(
+                        ExceptionResolver::getResolvableException,
+                        Function.identity()
+                ));
+        this.unexpectedExceptionResolvers = unexpectedExceptionResolvers;
+    }
+
+    @Override
+    public ExceptionDetail resolve(final Exception exception) {
+        try {
+            return Optional.ofNullable(expectedExceptionResolvers.get(exception.getClass()))
+                    .map(expectedExceptionResolver -> expectedExceptionResolver.resolve(exception))
+                    .orElseGet(() -> unexpectedExceptionResolvers.resolve(exception));
+        } catch (final Exception e) {
+            log.error("[FAIL TO RESOLVE] {}", exception.getMessage(), exception);
+            return ExceptionDetail.fromErrorCode(ErrorCode.FAIL_TO_HANDLE_EXCEPTION);
+        }
+    }
+
+    @Override
+    public Class<? extends Exception> getResolvableException() {
+        return Exception.class;
+    }
+}
