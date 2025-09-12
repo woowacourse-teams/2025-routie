92ef5a24e306405c5ea8a44bac4f2cf4afbc0bd3
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-13 02:16:58 +0900

    feat: HandlerMethod 에 파라메터 바인딩 시, 검증에 실패한 경우 발생하는 예외를 처리하는 Resolver 구현

---
+++ backend/spring-routie/src/main/java/routie/global/exception/infrastructure/resolver/expected/HandlerMethodValidationExceptionResolver.java
@@ -0,0 +1,26 @@
+package routie.global.exception.infrastructure.resolver.expected;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import routie.global.exception.domain.ErrorCode;
+import routie.global.exception.domain.ExceptionDetail;
+
+/**
+ * HandlerMethod 에 파라메터 바인딩 시, 검증에 실패한 경우 발생하는 예외를 처리하는 Resolver.
+ *
+ * <ul>
+ * <li>@Valid 어노테이션이 붙은 필드 검증이 실패했을 때.</li>
+ * </ul>
+ *
+ * @see org.springframework.web.method.HandlerMethod
+ * @see jakarta.validation.Valid
+ */
+@Component
+public class HandlerMethodValidationExceptionResolver extends
+        ExpectedExceptionResolver<HandlerMethodValidationException> {
+
+    @Override
+    protected ExceptionDetail resolveInternal(final HandlerMethodValidationException exception) {
+        return ExceptionDetail.fromErrorCode(ErrorCode.INVALID_REQUEST_DATA_VALUE);
+    }
+}
