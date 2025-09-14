c4d994cdb9a385dedca701a3ed67b25e43c06545
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-14 18:51:02 +0900

    feat: 예상 가능 예외 Resolver 가 중복으로 등록되지 않도록 보장
    - BeanDefinition 을 기반으로, Bean 초기화 시 수행.
    - 같은 예외 클래스에 대해 중복으로 등록된 ExpectedExceptionResolver 가 존재한다면, 애플리케이션이 실행되지 않음.

---
+++ backend/spring-routie/src/test/java/routie/global/exception/infrastructure/resolver/expected/ExpectedExceptionResolverDuplicateCheckerTest.java
@@ -0,0 +1,78 @@
+package routie.global.exception.infrastructure.resolver.expected;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import routie.global.exception.domain.ExceptionDetail;
+
+class ExpectedExceptionResolverDuplicateCheckerTest {
+
+    private final ExpectedExceptionResolverDuplicateChecker checker = new ExpectedExceptionResolverDuplicateChecker();
+    private final BeanDefinitionRegistry registry = new DefaultListableBeanFactory();
+
+    @Test
+    @DisplayName("중복된 ExpectedExceptionResolver가 없을 때, 정상적으로 실행된다.")
+    void noDuplicateResolversShouldExecuteSuccessfully() {
+        // given
+        registry.registerBeanDefinition("resolver1", BeanDefinitionBuilder
+                .rootBeanDefinition(TestResolver1.class)
+                .getBeanDefinition());
+
+        registry.registerBeanDefinition("resolver2", BeanDefinitionBuilder
+                .rootBeanDefinition(TestResolver2.class)
+                .getBeanDefinition());
+
+        // when & then
+        assertThatNoException()
+                .isThrownBy(() -> checker.postProcessBeanDefinitionRegistry(registry));
+    }
+
+    @Test
+    @DisplayName("중복된 ExpectedExceptionResolver가 있을 때, BeanCreationException이 발생한다.")
+    void duplicateResolversShouldThrowBeanCreationException() {
+        // given
+        registry.registerBeanDefinition("testResolver1", BeanDefinitionBuilder
+                .rootBeanDefinition(TestResolver1.class)
+                .getBeanDefinition());
+
+        registry.registerBeanDefinition("testResolver2", BeanDefinitionBuilder
+                .rootBeanDefinition(TestResolver2.class)
+                .getBeanDefinition());
+
+        registry.registerBeanDefinition("testResolver3", BeanDefinitionBuilder
+                .rootBeanDefinition(TestResolver3.class)
+                .getBeanDefinition());
+
+        // when & then
+        assertThatExceptionOfType(BeanCreationException.class)
+                .isThrownBy(() -> checker.postProcessBeanDefinitionRegistry(registry));
+    }
+
+    private static class TestResolver1 extends ExpectedExceptionResolver<IllegalArgumentException> {
+        @Override
+        protected ExceptionDetail resolveInternal(final IllegalArgumentException exception) {
+            return null;
+        }
+    }
+
+    private static class TestResolver2 extends ExpectedExceptionResolver<IllegalStateException> {
+        @Override
+        protected ExceptionDetail resolveInternal(final IllegalStateException exception) {
+            return null;
+        }
+    }
+
+    private static class TestResolver3 extends ExpectedExceptionResolver<IllegalArgumentException> {
+        @Override
+        protected ExceptionDetail resolveInternal(final IllegalArgumentException exception) {
+            return null;
+        }
+    }
+
+}
