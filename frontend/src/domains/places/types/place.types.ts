7925c90de4e031902deeb5551d702852dc15dbdd
ohgus <anakin1108@naver.com>
2025-09-15 21:05:08 +0900

    chore: PlaceList의 Context, Provider 데이터 제거

--- frontend/src/domains/places/types/place.types.ts
+++ frontend/src/domains/places/types/place.types.ts
@@ -14,18 +14,6 @@ interface PlaceDataType extends PlaceBaseType {
   id: number;
 }
 
-interface PlaceListContextType {
-  placeList: PlaceDataType[];
-  refetchPlaceList: () => Promise<void>;
-  handleDelete: (id: number) => void;
-  newlyAddedPlace: PlaceDataType | null;
-  handlePlaceAdded: () => Promise<void>;
-}
-
-interface PlaceListProviderProps {
-  children?: React.ReactNode;
-}
-
 type PlaceAdapterType = PlaceBaseType;
 
 type PlaceListAdapterType = PlaceDataType[];
@@ -36,8 +24,6 @@ export type {
   PlaceBaseType,
   SearchedPlaceType,
   PlaceDataType,
-  PlaceListContextType,
-  PlaceListProviderProps,
   PlaceAdapterType,
   PlaceListAdapterType,
   SearchPlaceAdapterType,
