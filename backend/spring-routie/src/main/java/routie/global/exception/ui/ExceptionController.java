fd019379e7843ff79c2ef7bbbccafe70a478cbcb
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-15 16:12:02 +0900

    chore: 사용하지 않는 `@Slf4j` 어노테이션 제거

--- backend/spring-routie/src/main/java/routie/global/exception/ui/ExceptionController.java
+++ backend/spring-routie/src/main/java/routie/global/exception/ui/ExceptionController.java
@@ -1,14 +1,12 @@
 package routie.global.exception.ui;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import routie.global.exception.domain.ExceptionDetail;
 import routie.global.exception.domain.ExceptionResolver;
 
-@Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ExceptionController {
@@ -16,8 +14,8 @@ public class ExceptionController {
     private final ExceptionResolver exceptionResolver;
 
     @ExceptionHandler(Exception.class)
-    public ProblemDetail handleException(final Exception ex) {
-        ExceptionDetail exceptionDetail = exceptionResolver.resolve(ex);
+    public ProblemDetail handleException(final Exception exception) {
+        ExceptionDetail exceptionDetail = exceptionResolver.resolve(exception);
         return exceptionDetail.toProblemDetail();
     }
 }
