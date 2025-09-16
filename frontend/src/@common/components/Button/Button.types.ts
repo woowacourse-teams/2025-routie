9ca30434d16b3dcc3cbe33677dc6105bae9fad98
ohgus <anakin1108@naver.com>
2025-09-16 16:14:19 +0900

    refactor: Button component Props 수정
    - justify-content: center 수정
    - padding: string 추가

--- frontend/src/@common/components/Button/Button.types.ts
+++ frontend/src/@common/components/Button/Button.types.ts
@@ -3,6 +3,7 @@ import { ComponentProps } from 'react';
 interface ButtonProps extends ComponentProps<'button'> {
   variant?: ButtonVariantType;
   width?: string;
+  padding?: string;
 }
 
 type ButtonVariantType = 'primary' | 'secondary';
