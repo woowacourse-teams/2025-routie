f3af93cd004c1a692bc3a660c3edfebc9e6bf9a7
ohgus <anakin1108@naver.com>
2025-09-16 18:11:29 +0900

    refactor: Pill 컴포넌트 리팩터링
    - 사용하지 않는 variant, type, style 제거
    - 현재 사용하는 곳이 한 곳이고 사용하는 스타일도 한 개여서 불필요한 코드를 모두 제거한다

--- frontend/src/@common/components/Pill/Pill.types.ts
+++ frontend/src/@common/components/Pill/Pill.types.ts
@@ -1,12 +1,5 @@
 import { ComponentProps } from 'react';
 
-interface PillProps extends ComponentProps<'div'> {
-  variant?: PillVariantType;
-  type: PillType;
-}
+type PillProps = ComponentProps<'div'>;
 
-type PillVariantType = 'default' | 'filled' | 'invalid';
-
-type PillType = 'time' | 'distance' | 'default';
-
-export type { PillProps, PillVariantType, PillType };
+export type { PillProps };
