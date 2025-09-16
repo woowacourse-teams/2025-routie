6b199445cfc326ca7bb9cbbc20ad56eb0d728757
AHHYUNJU <wndkgus0225@naver.com>
2025-09-16 20:15:05 +0900

    refactor: place.ts에서 domain utils에 routieSpaceUuid.ts 파일 추가

--- frontend/src/domains/places/apis/place.ts
+++ frontend/src/domains/places/apis/place.ts
@@ -15,22 +15,14 @@ import type {
   PlaceListAdapterType,
   SearchPlaceAdapterType,
 } from '@/domains/places/types/place.types';
-
-export const getSpaceUuid = (): string | null =>
-  typeof window !== 'undefined'
-    ? localStorage.getItem('routieSpaceUuid')
-    : null;
-
-export const ensureSpaceUuid = (): string => {
-  const uuid = getSpaceUuid();
-  if (!uuid) {
-    throw new Error('루티 스페이스 uuid가 없습니다.');
-  }
-  return uuid;
-};
+import {
+  ensureRoutieSpaceUuid,
+  getRoutieSpaceUuid,
+} from '@/domains/utils/routieSpaceUuid';
 
 const addPlace = async (placeInfo: AddPlaceRequestType) => {
-  const routieSpaceUuid = ensureSpaceUuid();
+  const routieSpaceUuid = getRoutieSpaceUuid();
+  ensureRoutieSpaceUuid(routieSpaceUuid);
 
   const response = await apiClient.post(
     `/routie-spaces/${routieSpaceUuid}/places`,
@@ -43,7 +35,8 @@ const addPlace = async (placeInfo: AddPlaceRequestType) => {
 };
 
 const deletePlace = async ({ placeId }: DeletePlaceRequestType) => {
-  const routieSpaceUuid = ensureSpaceUuid();
+  const routieSpaceUuid = getRoutieSpaceUuid();
+  ensureRoutieSpaceUuid(routieSpaceUuid);
 
   await apiClient.delete(`/routie-spaces/${routieSpaceUuid}/places/${placeId}`);
 };
@@ -51,7 +44,8 @@ const deletePlace = async ({ placeId }: DeletePlaceRequestType) => {
 const getPlace = async ({
   placeId,
 }: FetchPlaceRequestType): Promise<PlaceAdapterType> => {
-  const routieSpaceUuid = ensureSpaceUuid();
+  const routieSpaceUuid = getRoutieSpaceUuid();
+  ensureRoutieSpaceUuid(routieSpaceUuid);
 
   const response = await apiClient.get(
     `/routie-spaces/${routieSpaceUuid}/places/${placeId}`,
@@ -63,7 +57,8 @@ const getPlace = async ({
 };
 
 const getPlaceList = async (): Promise<PlaceListAdapterType> => {
-  const routieSpaceUuid = ensureSpaceUuid();
+  const routieSpaceUuid = getRoutieSpaceUuid();
+  ensureRoutieSpaceUuid(routieSpaceUuid);
 
   const response = await apiClient.get(
     `/routie-spaces/${routieSpaceUuid}/places`,
