2335777f4543a5bdb8f206ef247f8bb693309170
ohgus <anakin1108@naver.com>
2025-09-17 10:32:04 +0900

    feat: marker 렌더링 훅 구현

---
+++ frontend/src/domains/maps/hooks/useMarkerRenderer.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react';
+
+import { usePlaceList } from '@/domains/places/hooks/usePlaceList';
+import type { PlaceDataType } from '@/domains/places/types/place.types';
+
+import { useMapMarker } from './useMapMarker';
+import { useRoutePlacesWithDetails } from './useRoutePlacesWithDetails';
+
+import type { MapRefType } from '../types/KaKaoMap.types';
+
+interface UseMarkerRendererProps {
+  mapRef: MapRefType;
+  handleMarkerClick: (place: PlaceDataType) => void;
+}
+
+const useMarkerRenderer = ({
+  mapRef,
+  handleMarkerClick,
+}: UseMarkerRendererProps) => {
+  const { placeList } = usePlaceList();
+  const { routiePlacesWithDetails } = useRoutePlacesWithDetails();
+  const { clearMarkers, drawMarkers } = useMapMarker(mapRef);
+  const renderMarkers = useCallback(() => {
+    clearMarkers();
+    placeList?.forEach((place) => {
+      const routiePlace = routiePlacesWithDetails.find(rp => rp.id === place.id);
+      const routieSequence = routiePlace?.sequence;
+
+      drawMarkers({
+        place,
+        routieSequence,
+        onClick: () => handleMarkerClick(place),
+      });
+    });
+  }, [placeList, routiePlacesWithDetails, handleMarkerClick, clearMarkers, drawMarkers]);
+
+  return { renderMarkers };
+};
+
+export { useMarkerRenderer };
+export type { UseMarkerRendererProps };
