ac050bac5561048d265f57e444517d2b522a5459
AHHYUNJU <wndkgus0225@naver.com>
2025-09-24 08:55:42 +0900

    feat: ProfileButton 컴포넌트 구현

---
+++ frontend/src/domains/auth/components/ProfileButton/ProfileButton.styles.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+import theme from '@/styles/theme';
+
+const ProfileButtonStyle = css`
+  cursor: pointer;
+  width: fit-content;
+  background-color: ${theme.colors.white};
+`;
+
+export { ProfileButtonStyle };
---
+++ frontend/src/domains/auth/components/ProfileButton/ProfileButton.tsx
@@ -0,0 +1,23 @@
+import Button from '@/@common/components/Button/Button';
+import Flex from '@/@common/components/Flex/Flex';
+import Text from '@/@common/components/Text/Text';
+import Profile from '@/domains/auth/components/Profile/Profile';
+
+import { ProfileButtonStyle } from './ProfileButton.styles';
+
+interface ProfileButtonProps {
+  onClick: () => void;
+  text: string;
+}
+
+const ProfileButton = ({ onClick, text }: ProfileButtonProps) => {
+  return (
+    <Button css={ProfileButtonStyle} onClick={onClick}>
+      <Flex gap={1} width="1">
+        <Profile />
+        <Text variant="body">{text}</Text>
+      </Flex>
+    </Button>
+  );
+};
+export default ProfileButton;
