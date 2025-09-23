f9185bdb7c46727c5cad1340d26c0bf488fdf81a
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-09-23 16:05:38 +0900

    chore: 사용하지 않는 아이콘 이미지 제거 (#760)

--- frontend/src/@common/components/IconSvg/allIcons.ts
+++ frontend/src/@common/components/IconSvg/allIcons.ts
@@ -1,61 +1,37 @@
 import arrowWhite from '@/assets/icons/arrow-white.svg';
-import car from '@/assets/icons/car.svg';
 import checkHome from '@/assets/icons/check-home.svg';
 import check from '@/assets/icons/check.svg';
 import clockHome from '@/assets/icons/clock-home.svg';
-import clock from '@/assets/icons/clock.svg';
 import closeRed from '@/assets/icons/close-red.svg';
 import close from '@/assets/icons/close.svg';
 import closeSheetIcon from '@/assets/icons/closeSheet.svg';
 import copy from '@/assets/icons/copy.svg';
 import drag from '@/assets/icons/drag.svg';
-import durationTime from '@/assets/icons/duration-time.svg';
-import edit from '@/assets/icons/edit.svg';
-import fail from '@/assets/icons/fail.svg';
-import info from '@/assets/icons/info.svg';
-import loading from '@/assets/icons/loading.svg';
 import logo from '@/assets/icons/logo.png';
-import minus from '@/assets/icons/minus.svg';
 import pin from '@/assets/icons/pin.svg';
 import collectPlace from '@/assets/icons/place-home.svg';
 import plus from '@/assets/icons/plus.svg';
 import reload from '@/assets/icons/reload.svg';
 import search from '@/assets/icons/search.svg';
-import success from '@/assets/icons/success.svg';
-import timepass from '@/assets/icons/timepass.svg';
-import train from '@/assets/icons/train.svg';
 import disableTrash from '@/assets/icons/trash-disable.svg';
 import trash from '@/assets/icons/trash.svg';
-import waiting from '@/assets/icons/waiting.svg';
 
 const allIcons = {
   arrowWhite,
   check,
   checkHome,
-  clock,
   clockHome,
   close,
   closeSheetIcon,
   collectPlace,
   copy,
   drag,
-  edit,
-  fail,
-  info,
   logo,
-  loading,
-  minus,
   pin,
   plus,
   search,
-  success,
   trash,
-  timepass,
-  durationTime,
-  waiting,
   closeRed,
-  car,
-  train,
   disableTrash,
   reload,
 } as const;
