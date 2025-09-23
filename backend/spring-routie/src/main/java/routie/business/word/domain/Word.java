e98c26a7e296052e37537b443634f53721365c46
goohong <goohong.chung@gmail.com>
2025-09-23 21:17:09 +0900

    feat: Word 엔티티 추가

---
+++ backend/spring-routie/src/main/java/routie/business/word/domain/Word.java
@@ -0,0 +1,48 @@
+package routie.business.word.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Entity
+@Table(name = "words", uniqueConstraints = {
+        @UniqueConstraint(name = "UQ_word_type_content", columnNames = {"word_type", "content"})
+})
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "word_type", nullable = false, length = 20)
+    private WordType wordType;
+
+    @Column(name = "content", nullable = false, length = 100)
+    private String content;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public Word(final WordType wordType, final String content) {
+        this.wordType = wordType;
+        this.content = content;
+    }
+}
