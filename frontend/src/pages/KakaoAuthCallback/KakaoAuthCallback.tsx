c8231a650a57c29cef664c55f5373ac0b294309e
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-09-25 00:14:19 +0900

    refactor: alignItems 속성 기본값 적용하도록 변경

--- frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
+++ frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
@@ -4,7 +4,7 @@ import Text from '@/@common/components/Text/Text';
 
 const KakaoAuthCallback = () => {
   return (
-    <Flex direction="column" alignItems="center" height="100vh" gap={2}>
+    <Flex direction="column" height="100vh" gap={2}>
       <Spinner />
       <Text variant="title">로그인 중...</Text>
     </Flex>
