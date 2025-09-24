0898685e4149102c77c411fc9b0456609ad894ab
KimDaehyeon <daehyeon3351@gmail.com>
2025-09-24 21:48:36 +0900

    feat: externalAuthenticationIdentifier를 통해 User를 찾는 메서드 추가

--- backend/spring-routie/src/main/java/routie/business/user/domain/UserRepository.java
+++ backend/spring-routie/src/main/java/routie/business/user/domain/UserRepository.java
@@ -1,6 +1,9 @@
 package routie.business.user.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByExternalAuthenticationIdentifier(String externalAuthenticationIdentifier);
 }
