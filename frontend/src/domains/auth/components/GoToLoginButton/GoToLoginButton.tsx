5100b1b7fe29b323c3ee6beb46d6a095fc598bdd
ohgus <anakin1108@naver.com>
2025-09-24 15:07:47 +0900

    refactor: type 파일 분리
    - 줄바꿈 추가

--- frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.tsx
+++ frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.tsx
@@ -1,18 +1,16 @@
 import Button from '@/@common/components/Button/Button';
-import Flex from '@/@common/components/Flex/Flex';
 import Text from '@/@common/components/Text/Text';
 
 import { GoToLoginButtonStyle } from './GoToLoginButton.styles';
 
-interface LoginButtonProps {
-  onClick: () => void;
-}
+import type { GoToLoginButtonProps } from './GoToLoginButton.types';
 
-const GoToLoginButton = ({ onClick }: LoginButtonProps) => {
+const GoToLoginButton = ({ onClick }: GoToLoginButtonProps) => {
   return (
     <Button css={GoToLoginButtonStyle} onClick={onClick} padding="2rem">
       <Text variant="title">로그인하고 동선 만들러 가기</Text>
     </Button>
   );
 };
+
 export default GoToLoginButton;
