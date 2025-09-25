cba08454dfe3a0b0dd43d738e9312bfd52dfd77e
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-09-25 14:36:58 +0900

    feat: 로그인 실패 시 홈으로 가기 버튼 표시 기능 구현

--- frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
+++ frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
@@ -1,12 +1,47 @@
+import { useEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
+
 import Flex from '@/@common/components/Flex/Flex';
 import Spinner from '@/@common/components/Spinner/Spinner';
 import Text from '@/@common/components/Text/Text';
+import { useKakaoLoginMutation } from '@/domains/auth/queries/useAuthQuery';
 
 const KakaoAuthCallback = () => {
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const code = searchParams.get('code');
+
+  const { mutate: kakaoAccessTokenMutate, isError } = useKakaoLoginMutation();
+
+  const handleGoHome = () => {
+    navigate('/', { replace: true });
+  };
+
+  useEffect(() => {
+    const handleKakaoLogin = async () => {
+      if (!code) {
+        navigate('/', { replace: true });
+        return;
+      }
+      kakaoAccessTokenMutate(code);
+    };
+    handleKakaoLogin();
+  }, []);
+
   return (
     <Flex direction="column" height="100vh" gap={2}>
-      <Spinner />
-      <Text variant="title">로그인 중...</Text>
+      {isError ? (
+        <Flex direction="column" alignItems="center" gap={2}>
+          <Text variant="title">로그인 실패</Text>{' '}
+          <button onClick={handleGoHome}>홈으로 가기</button>
+        </Flex>
+      ) : (
+        <>
+          <Spinner />
+          <Text variant="title">로그인 중...</Text>
+        </>
+      )}
     </Flex>
   );
 };
