cf1ef319e906ec7d54b5fd4ea326e461b764857d
DongchannN <ldc991104@gmail.com>
2025-09-29 19:40:30 +0900

    feat: 장소 좋아요 엔티티 구현

---
+++ backend/spring-routie/src/main/java/routie/business/placelike/domain/PlaceLike.java
@@ -0,0 +1,44 @@
+package routie.business.placelike.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import routie.business.place.domain.Place;
+
+@Entity
+@Getter
+@Table(name = "place_likes")
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaceLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id", nullable = false)
+    private Place place;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public PlaceLike(final Place place) {
+        this.place = place;
+    }
+}
