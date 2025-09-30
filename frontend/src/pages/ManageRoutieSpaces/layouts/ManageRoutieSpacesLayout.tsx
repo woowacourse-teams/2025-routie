fe9ca92a7f81be642be03fd21a517fce984f79d1
ohgus <anakin1108@naver.com>
2025-09-30 15:45:15 +0900

    feat: 사용자 루티 스페이스 관리 페이지 레이아웃 구현

---
+++ frontend/src/pages/ManageRoutieSpaces/layouts/ManageRoutieSpacesLayout.tsx
@@ -0,0 +1,25 @@
+import Flex from '@/@common/components/Flex/Flex';
+
+import { ManageRoutieSpacesLayoutStyle } from './ManageRoutiSpacesLayout.styles';
+
+const ManageRoutieSpacesLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <Flex
+      direction="column"
+      gap={3}
+      width="70%"
+      alignItems="flex-start"
+      padding="1rem 0"
+      margin="0 auto"
+      css={ManageRoutieSpacesLayoutStyle}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+export default ManageRoutieSpacesLayout;
