ab2182711ae3997a717c7223e3e529865cc55e8e
DongchannN <96824025+DongchannN@users.noreply.github.com>
2025-10-03 00:24:57 +0900

    fix: 루티 스페이스 삭제 시 장소에 관련된 좋아요 삭제 (#855)

--- backend/spring-routie/src/main/java/routie/business/like/domain/PlaceLikeRepository.java
+++ backend/spring-routie/src/main/java/routie/business/like/domain/PlaceLikeRepository.java
@@ -1,6 +1,8 @@
 package routie.business.like.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import routie.business.place.domain.Place;
 
@@ -10,4 +12,8 @@ public interface PlaceLikeRepository extends JpaRepository<PlaceLike, Long> {
     long countByPlace(Place place);
 
     void deleteByPlaceId(long placeId);
+
+    @Modifying
+    @Query("DELETE FROM PlaceLike pl WHERE pl.place.routieSpace.id = :routieSpaceId")
+    void deleteByRoutieSpaceId(long routieSpaceId);
 }
