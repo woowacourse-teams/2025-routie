1407c1c748e0d324260a4052095104b8b79f2401
goohong <goohong.chung@gmail.com>
2025-10-03 21:04:10 +0900

    feat: PlaceLike에 User, Guest 필드 추가

--- backend/spring-routie/src/main/java/routie/business/like/domain/PlaceLike.java
+++ backend/spring-routie/src/main/java/routie/business/like/domain/PlaceLike.java
@@ -16,6 +16,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import routie.business.participant.domain.Guest;
+import routie.business.participant.domain.User;
 import routie.business.place.domain.Place;
 
 @Entity
@@ -34,11 +36,29 @@ public class PlaceLike {
     @JoinColumn(name = "place_id", nullable = false)
     private Place place;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "guest_id")
+    private Guest guest;
+
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
-    public PlaceLike(final Place place) {
+    public PlaceLike(final Place place, final User user, final Guest guest) {
         this.place = place;
+        this.user = user;
+        this.guest = guest;
+    }
+
+    public static PlaceLike of(final Place place, final User user) {
+        return new PlaceLike(place, user, null);
+    }
+
+    public static PlaceLike of(final Place place, final Guest guest) {
+        return new PlaceLike(place, null, guest);
     }
 }
