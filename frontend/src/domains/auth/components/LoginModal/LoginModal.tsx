c06fa918c41b8f8603e51d465ace0dc12e8c01ee
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-10-04 21:21:43 +0900

    refactor: 소셜 로그인 컴포넌트 분리

--- frontend/src/domains/auth/components/LoginModal/LoginModal.tsx
+++ frontend/src/domains/auth/components/LoginModal/LoginModal.tsx
@@ -3,39 +3,11 @@ import type { ModalProps } from '@/@common/components/Modal/Modal.types';
 import ModalLayout from '@/@common/components/ModalLayout/ModalLayout';
 import Text from '@/@common/components/Text/Text';
 import { useToastContext } from '@/@common/contexts/useToastContext';
-import kakaoImage from '@/assets/images/kakao.png';
+import SocialLoginSection from '../SocialLoginSection/SocialLoginSection';
 
-import { useKakaoLoginUriQuery } from '../../queries/useAuthQuery';
-
-import {
-  LoginModalStyle,
-  KakaoButtonStyle,
-  DividerStyle,
-} from './LoginModal.styles';
+import { LoginModalStyle } from './LoginModal.styles';
 
 const LoginModal = ({ onClose }: Pick<ModalProps, 'onClose'>) => {
-  const { data: kakaoLoginUri } = useKakaoLoginUriQuery();
-  const { showToast } = useToastContext();
-
-  const redirectToKakaoLogin = async () => {
-    try {
-      if (kakaoLoginUri?.uri) {
-        window.open(kakaoLoginUri.uri, '_self');
-      }
-      onClose();
-    } catch (err: any) {
-      showToast({
-        message: err?.message,
-        type: 'error',
-      });
-    }
-  };
-
-  const handleKakaoLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
-    redirectToKakaoLogin();
-  };
-
   return (
     <ModalLayout onClose={onClose} showCloseButton={true} width="36rem">
       <div css={LoginModalStyle}>
@@ -45,14 +17,7 @@ const LoginModal = ({ onClose }: Pick<ModalProps, 'onClose'>) => {
             <Text variant="subTitle">동선을 손쉽게 완성하세요!</Text>
           </Flex>
           <Flex direction="column" gap={3}>
-            <Flex gap={1}>
-              <div css={DividerStyle} />
-              <Text variant="caption">SNS 계정으로 간편로그인</Text>
-              <div css={DividerStyle} />
-            </Flex>
-            <a href="#" onClick={handleKakaoLinkClick} css={KakaoButtonStyle}>
-              <img src={kakaoImage} alt="카카오 로그인" />
-            </a>
+            <SocialLoginSection onClose={onClose} />
           </Flex>
         </Flex>
       </div>
---
+++ frontend/src/domains/auth/components/SocialLoginSection/SocialLoginSection.tsx
@@ -0,0 +1,49 @@
+import Flex from '@/@common/components/Flex/Flex';
+import Text from '@/@common/components/Text/Text';
+import { useToastContext } from '@/@common/contexts/useToastContext';
+import kakaoImage from '@/assets/images/kakao.png';
+import { useKakaoLoginUriQuery } from '@/domains/auth/queries/useAuthQuery';
+
+import { DividerStyle, KakaoButtonStyle } from './SocialLoginSection.styles';
+
+interface SocialLoginSectionProps {
+  onClose: () => void;
+}
+
+const SocialLoginSection = ({ onClose }: SocialLoginSectionProps) => {
+  const { data: kakaoLoginUri } = useKakaoLoginUriQuery();
+  const { showToast } = useToastContext();
+
+  const redirectToKakaoLogin = async () => {
+    try {
+      if (kakaoLoginUri?.uri) {
+        window.open(kakaoLoginUri.uri, '_self');
+      }
+      onClose();
+    } catch (err: any) {
+      showToast({
+        message: err?.message,
+        type: 'error',
+      });
+    }
+  };
+
+  const handleKakaoLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    redirectToKakaoLogin();
+  };
+  return (
+    <>
+      <Flex gap={1}>
+        <div css={DividerStyle} />
+        <Text variant="caption">SNS 계정으로 간편로그인</Text>
+        <div css={DividerStyle} />
+      </Flex>
+      <a href="#" onClick={handleKakaoLinkClick} css={KakaoButtonStyle}>
+        <img src={kakaoImage} alt="카카오 로그인" />
+      </a>
+    </>
+  );
+};
+
+export default SocialLoginSection;
