e54b506a8d6df55bbfce146d7c0db5d492fd493a
goohong <goohong.chung@gmail.com>
2025-10-06 15:03:49 +0900

    refactor: 메서드 파라미터명 명확하게 수정

--- backend/spring-routie/src/main/java/routie/business/participant/domain/GuestRepository.java
+++ backend/spring-routie/src/main/java/routie/business/participant/domain/GuestRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuestRepository extends JpaRepository<Guest, Long> {
-    boolean existsByNicknameAndRoutieSpaceId(String nickname, Long id);
+    boolean existsByNicknameAndRoutieSpaceId(String nickname, Long routieSpaceId);
 
     Optional<Guest> findByNicknameAndRoutieSpaceId(String nickname, Long RoutieSpaceId);
 }
