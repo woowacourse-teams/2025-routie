cd04c0eca94c20cca1d2dc098c21e19ffbf55a9b
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-10-06 19:05:57 +0900

    feat: 좋아요 눌렸을 때 스타일 추가

--- frontend/src/domains/places/components/LikeButton/LikeButton.styles.ts
+++ frontend/src/domains/places/components/LikeButton/LikeButton.styles.ts
@@ -2,17 +2,21 @@ import { css } from '@emotion/react';
 
 import theme from '@/styles/theme';
 
-const LikeButtonStyle = css`
+const LikeButtonStyle = (liked: boolean) => css`
   all: unset;
 
   cursor: pointer;
 
   padding: 0.2rem 0.5rem;
-  border: 1px solid ${theme.colors.gray[100]};
+  border: 1px solid ${liked ? theme.colors.purple[300] : theme.colors.gray[100]};
   border-radius: 8px;
 
+  background-color: ${liked ? theme.colors.purple[50] : theme.colors.white};
+
   &:hover {
-    background-color: ${theme.colors.gray[25]};
+    background-color: ${liked
+      ? theme.colors.purple[100]
+      : theme.colors.gray[25]};
   }
 `;
 
