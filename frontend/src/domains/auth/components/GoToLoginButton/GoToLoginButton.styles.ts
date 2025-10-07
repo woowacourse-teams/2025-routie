04d31359a040adfae96acf4a7873b9060f2702ae
AHHYUNJU <wndkgus0225@naver.com>
2025-10-07 14:35:12 +0900

    refactor: 동선 만들러가기 버튼 수정
    - text 문구 수정
    - 변경된 text 공통 컴포넌트 사용

--- frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.styles.ts
+++ frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.styles.ts
@@ -1,10 +1,8 @@
 import { css } from '@emotion/react';
 
-import theme from '@/styles/theme';
-
 const GoToLoginButtonStyle = css`
   width: 50rem;
-  background-color: ${theme.colors.white};
+  padding: 2rem;
   box-shadow: 0 0.8rem 1.6rem rgb(0 0 0 / 26%);
 `;
 export { GoToLoginButtonStyle };
--- frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.tsx
+++ frontend/src/domains/auth/components/GoToLoginButton/GoToLoginButton.tsx
@@ -1,5 +1,6 @@
 import Button from '@/@common/components/Button/Button';
 import Text from '@/@common/components/Text/Text';
+import theme from '@/styles/theme';
 
 import { GoToLoginButtonStyle } from './GoToLoginButton.styles';
 
@@ -7,8 +8,10 @@ import type { GoToLoginButtonProps } from './GoToLoginButton.types';
 
 const GoToLoginButton = ({ onClick }: GoToLoginButtonProps) => {
   return (
-    <Button css={GoToLoginButtonStyle} onClick={onClick} padding="2rem">
-      <Text variant="title">로그인하고 동선 만들러 가기</Text>
+    <Button variant="primary" css={GoToLoginButtonStyle} onClick={onClick}>
+      <Text color={theme.colors.white} variant="title">
+        친구들과 동선 만들러 가기
+      </Text>
     </Button>
   );
 };
