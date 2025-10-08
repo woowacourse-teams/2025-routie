3cd3572ac44d2af108fa80fe6fbbea239e36e723
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-10-08 16:25:33 +0900

    fix: 로그아웃 시 페이지를 리로드하도록 수정
    - 기존에는 홈으로 리다이렉트 했는데 기존 페이지에 머무르도록 하는 것이 더 적절한 방법이라고 생각함

--- frontend/src/domains/auth/hooks/useLogout.ts
+++ frontend/src/domains/auth/hooks/useLogout.ts
@@ -1,12 +1,8 @@
-import { useNavigate } from 'react-router';
-
 const useLogout = () => {
-  const navigate = useNavigate();
-
   const handleLogout = () => {
     localStorage.removeItem('accessToken');
     localStorage.removeItem('role');
-    navigate('/', { replace: true });
+    window.location.reload();
     return;
   };
   return { handleLogout };
