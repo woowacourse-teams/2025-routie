15fde6cf85289c60fc12a9a84e5ae281ea09fd8c
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-10-08 15:38:47 +0900

    fix: 카카오 로그인 실패 시 홈으로 리다이렉트 하지 않고 UI르 바꾸도록 수정
    - 홈으로 리다이렉트 하면 로그인이 실패했는지 안내를 사용자에게 해줄 수 없어 이 로직을 제거함

--- frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
+++ frontend/src/pages/KakaoAuthCallback/KakaoAuthCallback.tsx
@@ -30,14 +30,12 @@ const KakaoAuthCallback = () => {
         handleGoHome();
         return;
       }
-      (async () => {
-        try {
-          await kakaoAccessTokenMutate(code);
-          redirectAfterLogin();
-        } catch {
-          handleGoHome();
-        }
-      })();
+      try {
+        await kakaoAccessTokenMutate(code);
+        redirectAfterLogin();
+      } catch {
+        console.error('카카오 로그인 중 에러 발생');
+      }
     };
     handleKakaoLogin();
   }, []);
