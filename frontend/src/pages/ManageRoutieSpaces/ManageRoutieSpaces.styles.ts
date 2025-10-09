51de1150043e5de5491ccf86b6cb0cc0081a9226
ohgus <anakin1108@naver.com>
2025-10-09 21:18:44 +0900

    refactor: 루티 스페이스 관리 페이지 반응형 step 추가 및 각 step grid 수정

--- frontend/src/pages/ManageRoutieSpaces/ManageRoutieSpaces.styles.ts
+++ frontend/src/pages/ManageRoutieSpaces/ManageRoutieSpaces.styles.ts
@@ -9,15 +9,27 @@ const ManageRoutieSpacesStyle = css`
 
 const RoutieSpaceListStyle = css`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(6, 1fr);
   gap: 2rem;
   width: 100%;
 
+  @media (width <= 2000px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media (width <= 1800px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
   @media (width <= 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (width <= 820px) {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media (width <= 800px) {
+  @media (width <= 540px) {
     grid-template-columns: repeat(1, 1fr);
   }
 `;
