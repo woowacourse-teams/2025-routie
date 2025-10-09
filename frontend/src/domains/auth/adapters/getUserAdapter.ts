9d27b0dbff3a8334f756a200fbc8b9b18d991ad7
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-10-09 23:05:08 +0900

    refactor: getUserAdapter 함수 export 위치 변경

--- frontend/src/domains/auth/adapters/getUserAdapter.ts
+++ frontend/src/domains/auth/adapters/getUserAdapter.ts
@@ -1,6 +1,8 @@
 import type { UserResponseType } from '@/domains/auth/types/api.types';
 import type { UserType } from '@/domains/auth/types/user.types';
 
-export const getUserAdapter = (data: UserResponseType): UserType => {
+const getUserAdapter = (data: UserResponseType): UserType => {
   return { nickName: data.nickname, role: data.role };
 };
+
+export { getUserAdapter };
