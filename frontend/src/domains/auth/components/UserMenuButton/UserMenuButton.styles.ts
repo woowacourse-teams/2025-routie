783af67bf348f2c3b41d7646e86d01fc6ed2c1ee
ohgus <anakin1108@naver.com>
2025-10-10 20:35:11 +0900

    refactor: UserMenuButton 스타일 변경
    - 새로운 아이콘 적용
    - 스타일 변경 (radius, shadow, padding 등)

--- frontend/src/domains/auth/components/UserMenuButton/UserMenuButton.styles.ts
+++ frontend/src/domains/auth/components/UserMenuButton/UserMenuButton.styles.ts
@@ -4,12 +4,12 @@ import theme from '@/styles/theme';
 
 const UserMenuIconStyle = css`
   cursor: pointer;
+  padding: 0.8rem;
+  border-radius: ${theme.radius.sm};
 
-  padding: 0.4rem;
-  border-radius: 50%;
-
-  background-color: ${theme.colors.white};
-  box-shadow: 0 0.4rem 0.8rem rgb(0 0 0 / 25%);
+  &:hover {
+    background-color: ${theme.colors.blue[200]};
+  }
 `;
 
 const UserMenuButtonWrapperStyle = css`
