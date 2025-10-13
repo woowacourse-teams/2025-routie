21f918b3573e747e74f0fd135daaa3e66418b376
AHHYUNJU <wndkgus0225@naver.com>
2025-10-14 00:07:22 +0900

    feat: 채팅 헤더 및 푸터 스타일 추가

--- frontend/src/pages/Home/PhoneChatFrame/PhoneChatFrame.styles.ts
+++ frontend/src/pages/Home/PhoneChatFrame/PhoneChatFrame.styles.ts
@@ -9,15 +9,34 @@ const PhoneContainerStyle = css`
 const ScreenBoxStyle = css`
   position: absolute;
   inset: 3.5% 2.5%;
+
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
+
+  height: calc(100% - 7%);
   border-radius: 3.5rem;
+`;
+
+const ChatHeaderStyle = css`
+  display: block;
+  flex-shrink: 0;
+  width: 100%;
+  height: auto;
 `;
 
 const ChatScrollStyle = css`
   scrollbar-width: none;
   overflow: hidden auto;
+  flex: 1;
   width: 100%;
-  height: 100%;
+`;
+
+const ChatFooterStyle = css`
+  display: block;
+  flex-shrink: 0;
+  width: 100%;
+  height: auto;
 `;
 
 const ChatImageStyle = css`
@@ -42,7 +61,9 @@ const FrameOverlayStyle = css`
 export {
   PhoneContainerStyle,
   ScreenBoxStyle,
+  ChatHeaderStyle,
   ChatScrollStyle,
+  ChatFooterStyle,
   ChatImageStyle,
   FrameOverlayStyle,
 };
--- frontend/src/pages/Home/PhoneChatFrame/PhoneChatFrame.tsx
+++ frontend/src/pages/Home/PhoneChatFrame/PhoneChatFrame.tsx
@@ -1,13 +1,17 @@
 import Flex from '@/@common/components/Flex/Flex';
 import Icon from '@/@common/components/IconSvg/Icon';
 import Text from '@/@common/components/Text/Text';
+import chatFooter from '@/assets/images/chat-footer.svg';
+import chatHeader from '@/assets/images/chat-header.svg';
 import phoneFrame from '@/assets/images/iphone-frame.png';
 import kakaoChatImage from '@/assets/images/kakao-chat.svg';
 
 import {
   PhoneContainerStyle,
   ScreenBoxStyle,
+  ChatHeaderStyle,
   ChatScrollStyle,
+  ChatFooterStyle,
   ChatImageStyle,
   FrameOverlayStyle,
 } from './PhoneChatFrame.styles';
@@ -21,9 +25,11 @@ const PhoneFrame = () => {
       </Flex>
       <div css={PhoneContainerStyle}>
         <div css={ScreenBoxStyle}>
+          <img src={chatHeader} alt="카톡 헤더" css={ChatHeaderStyle} />
           <div css={ChatScrollStyle}>
             <img src={kakaoChatImage} alt="카톡 대화창" css={ChatImageStyle} />
           </div>
+          <img src={chatFooter} alt="카톡 푸터" css={ChatFooterStyle} />
         </div>
 
         <img src={phoneFrame} alt="아이폰 프레임" css={FrameOverlayStyle} />
