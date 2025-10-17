b67d2e2ee63e016708e9e4dcf0f6c1a4d1182d02
AHHYUNJU <wndkgus0225@naver.com>
2025-10-17 15:18:36 +0900

    refactor: 불필요한 로직 삭제 및 export 방식 변경

--- frontend/src/@common/utils/format.ts
+++ frontend/src/@common/utils/format.ts
@@ -1,35 +1,5 @@
-export const getKoreanCurrentDateISO = () => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const day = String(now.getDate()).padStart(2, '0');
-
-  return `${year}-${month}-${day}`;
-};
-
-export const getCurrentTimeHM = () => {
-  const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-
-  return `${hours}:${minutes}`;
-};
-
-export const getCombineDateTime = (dateTime: {
-  date: string;
-  startTime: string;
-  endTime: string;
-}) => {
-  if (!dateTime.date || !dateTime.startTime || !dateTime.endTime) {
-    return { startDateTime: '', endDateTime: '' };
-  }
-
-  return {
-    startDateTime: `${dateTime.date}T${dateTime.startTime}:00`,
-    endDateTime: `${dateTime.date}T${dateTime.endTime}:00`,
-  };
-};
-
-export const formatHashtag = (tag: string): string => {
+const formatHashtag = (tag: string): string => {
   return tag.startsWith('#') ? tag : `#${tag}`;
 };
+
+export { formatHashtag };
