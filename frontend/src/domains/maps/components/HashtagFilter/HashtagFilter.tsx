2d5add24ef6610ec40cadd255346a45b3d64cbd2
AHHYUNJU <wndkgus0225@naver.com>
2025-10-21 03:08:12 +0900

    feat: 전체 선택/해제 버튼 기능 및 스타일 추가

--- frontend/src/domains/maps/components/HashtagFilter/HashtagFilter.tsx
+++ frontend/src/domains/maps/components/HashtagFilter/HashtagFilter.tsx
@@ -1,10 +1,13 @@
 import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
+import Icon from '@/@common/components/IconSvg/Icon';
+import Text from '@/@common/components/Text/Text';
 import { useToggle } from '@/@common/hooks/useToggle';
 import { useHashtagFilter } from '@/domains/maps/hooks/useHashtagFilter';
 import Hashtag from '@/domains/places/components/Hashtag/Hashtag';
 
 import {
+  ButtonStyle,
   ButtonContainerStyle,
   HashtagsContainerStyle,
   DropdownWrapperStyle,
@@ -22,14 +25,28 @@ const HashtagFilter = ({ isSidebarOpen }: HashtagFilterProps) => {
     visibleHashtags,
     hiddenHashtags,
     handleHashtagClick,
+    isAllSelected,
+    handleToggleSelectAll,
   } = useHashtagFilter();
-  const { isOpen: isDropdownOpen, handleToggle: handleDropdownToggle } = useToggle(false);
+  const { isOpen: isDropdownOpen, handleToggle: handleDropdownToggle } =
+    useToggle(false);
 
   if (hashtags.length === 0) return null;
 
   return (
     <Flex width="auto" gap={0.8} css={ButtonContainerStyle(isSidebarOpen)}>
       <div ref={visibleHashtagsRef} css={HashtagsContainerStyle}>
+        <Button
+          variant={isAllSelected ? 'primary' : 'secondary'}
+          width="auto"
+          onClick={handleToggleSelectAll}
+          css={ButtonStyle(isAllSelected)}
+        >
+          <Icon name={isAllSelected ? 'allUnselect' : 'allSelect'} size={20} />
+          <Text variant="body" color={isAllSelected ? 'white' : undefined}>
+            {isAllSelected ? '전체 해제' : '전체 선택'}
+          </Text>
+        </Button>
         {visibleHashtags.map((hashtag) => (
           <Hashtag
             key={hashtag}
@@ -38,32 +55,33 @@ const HashtagFilter = ({ isSidebarOpen }: HashtagFilterProps) => {
             onClick={() => handleHashtagClick(hashtag)}
           />
         ))}
+        {hiddenHashtags.length > 0 && (
+          <div css={DropdownWrapperStyle}>
+            <Button
+              variant="secondary"
+              onClick={handleDropdownToggle}
+              css={DropdownButtonStyle(isDropdownOpen)}
+            >
+              +{hiddenHashtags.length}
+            </Button>
+            <Flex
+              direction="column"
+              gap={0.4}
+              padding={0.8}
+              css={DropdownContentStyle(isDropdownOpen)}
+            >
+              {hiddenHashtags.map((hashtag) => (
+                <Hashtag
+                  key={hashtag}
+                  tag={hashtag}
+                  isSelected={selectedHashtags.includes(hashtag)}
+                  onClick={() => handleHashtagClick(hashtag)}
+                />
+              ))}
+            </Flex>
+          </div>
+        )}
       </div>
-      {hiddenHashtags.length > 0 && (
-        <div css={DropdownWrapperStyle}>
-          <Button
-            variant="secondary"
-            onClick={handleDropdownToggle}
-            css={DropdownButtonStyle(isDropdownOpen)}
-          >
-            +{hiddenHashtags.length}
-          </Button>
-          <Flex
-            direction="column"
-            gap={0.4}
-            css={DropdownContentStyle(isDropdownOpen)}
-          >
-            {hiddenHashtags.map((hashtag) => (
-              <Hashtag
-                key={hashtag}
-                tag={hashtag}
-                isSelected={selectedHashtags.includes(hashtag)}
-                onClick={() => handleHashtagClick(hashtag)}
-              />
-            ))}
-          </Flex>
-        </div>
-      )}
     </Flex>
   );
 };
