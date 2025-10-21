d89a433f9b21e6e6122807b2b20ac5605cf82e77
ohgus <123801385+ohgus@users.noreply.github.com>
2025-10-21 14:34:22 +0900

    refactor: 루티 스페이스 관리 페이지 카드 UI (#938)

--- frontend/src/pages/ManageRoutieSpaces/components/CreateRoutieSpaceButton/CreateRoutieSpaceButton.tsx
+++ frontend/src/pages/ManageRoutieSpaces/components/CreateRoutieSpaceButton/CreateRoutieSpaceButton.tsx
@@ -1,10 +1,15 @@
 /** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 
 import Button from '@/@common/components/Button/Button';
 import Flex from '@/@common/components/Flex/Flex';
 import Icon from '@/@common/components/IconSvg/Icon';
 import Text from '@/@common/components/Text/Text';
+
+import {
+  ButtonContentContainerStyle,
+  ButtonIconSectionStyle,
+  CreateRoutieSpaceButtonStyle,
+} from './CreateRoutieSpaceButton.styles';
 
 import type { CreateRoutieSpaceButtonProps } from './CreateRoutieSpaceButton.types';
 
@@ -13,38 +18,16 @@ const CreateRoutieSpaceButton = ({ onClick }: CreateRoutieSpaceButtonProps) => {
     <Button
       variant="primary"
       onClick={onClick}
-      css={css`
-        max-width: 25rem;
-
-        @media (width <= 540px) {
-          max-width: 100%;
-        }
-      `}
+      css={CreateRoutieSpaceButtonStyle}
     >
       <Flex
         direction="column"
         gap={1.5}
         padding={1}
         height="22rem"
-        css={css`
-          @media (width <= 540px) {
-            flex-direction: row;
-            gap: 1rem;
-            width: 100%;
-            height: auto;
-          }
-        `}
+        css={ButtonContentContainerStyle}
       >
-        <Icon
-          name="plusRound"
-          size={80}
-          css={css`
-            @media (width <= 540px) {
-              width: 40px;
-              height: 40px;
-            }
-          `}
-        />
+        <Icon name="plusRound" size={80} css={ButtonIconSectionStyle} />
         <Text variant="subTitle" color="white">
           새 동선 만들기
         </Text>
