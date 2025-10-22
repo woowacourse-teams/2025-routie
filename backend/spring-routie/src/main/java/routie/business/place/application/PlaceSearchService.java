0b857d2f46356c9ba37dedeb4810caf9195da1f2
goohong <goohong.chung@gmail.com>
2025-10-22 13:12:57 +0900

    refactor: 검색 결과 수 15개로 조정

--- backend/spring-routie/src/main/java/routie/business/place/application/PlaceSearchService.java
+++ backend/spring-routie/src/main/java/routie/business/place/application/PlaceSearchService.java
@@ -1,19 +1,20 @@
 package routie.business.place.application;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import routie.business.place.ui.dto.response.SearchedPlacesResponse;
 import routie.business.place.domain.PlaceSearcher;
 import routie.business.place.domain.SearchedPlace;
+import routie.business.place.ui.dto.response.SearchedPlacesResponse;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PlaceSearchService {
 
-    private static final int SEARCH_SIZE = 5;
+    private static final int SEARCH_SIZE = 15;
     private final PlaceSearcher placeSearcher;
 
     public SearchedPlacesResponse search(final String query) {
