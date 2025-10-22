c1b8135e487831bd5925505679527b829382dc3f
jeongyou <wjddbwjd1205@naver.com>
2025-10-22 20:41:02 +0900

    feat: 동선 SSE 이벤트에 장소명 토스트 표시

---
+++ frontend/src/domains/routie/types/routieStream.types.ts
@@ -0,0 +1,19 @@
+import { FetchRoutieResponseType } from './api.types';
+
+type RoutieHistoryEvent = FetchRoutieResponseType;
+
+interface RoutieCreatedEvent extends FetchRoutieResponseType {
+  createdRoutiePlaceId: number;
+}
+type RoutieUpdatedEvent = FetchRoutieResponseType;
+
+interface RoutieDeletedEvent extends FetchRoutieResponseType {
+  deletedRoutiePlaceId: number;
+}
+
+export {
+  RoutieHistoryEvent,
+  RoutieCreatedEvent,
+  RoutieUpdatedEvent,
+  RoutieDeletedEvent,
+};
