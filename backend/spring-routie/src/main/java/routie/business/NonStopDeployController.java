f1cb3af4ce4e909d1756733974c893365a4b88ec
KimDaehyeon <daehyeon3351@gmail.com>
2025-10-23 11:54:37 +0900

    test: 첫 번째 무중단 배포 테스트

---
+++ backend/spring-routie/src/main/java/routie/business/NonStopDeployController.java
@@ -0,0 +1,13 @@
+package routie.business;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NonStopDeployController {
+
+    @GetMapping("/non-stop-deploy/test")
+    public String test() {
+        return "test1";
+    }
+}
