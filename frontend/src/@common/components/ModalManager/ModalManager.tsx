42cc78bcf3628603b1ef6b523268b42f06578e61
ohgus <123801385+ohgus@users.noreply.github.com>
2025-10-23 19:56:18 +0900

    refactor: 비회원 로그인 모달에서 닫기 버튼 제거 (#1009)

--- frontend/src/@common/components/ModalManager/ModalManager.tsx
+++ frontend/src/@common/components/ModalManager/ModalManager.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
+
 import Modal from '@/@common/components/Modal/Modal';
 import { useModal } from '@/@common/contexts/ModalContext';
 import LoginModal from '@/domains/auth/components/LoginModal/LoginModal';
@@ -5,6 +8,14 @@ import SocialLoginModal from '@/domains/auth/components/LoginModal/SocialLoginMo
 
 const ModalManager = () => {
   const { modalState, closeModal } = useModal();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (modalState.isOpen) {
+      closeModal();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
 
   const renderModalContent = () => {
     switch (modalState.type) {
@@ -18,7 +29,11 @@ const ModalManager = () => {
   };
 
   return (
-    <Modal isOpen={modalState.isOpen} onClose={closeModal}>
+    <Modal
+      isOpen={modalState.isOpen}
+      onClose={closeModal}
+      closable={modalState.type !== 'login'}
+    >
       {renderModalContent()}
     </Modal>
   );
