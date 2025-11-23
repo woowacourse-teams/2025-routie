9639284d3a1ea08f651aa916a27ac0b7ba3ee50c
DongchannN <ldc991104@gmail.com>
2025-11-23 20:06:52 +0900

    style: 불필요한 import 문 제거

--- backend/spring-routie/src/main/java/routie/business/routie/application/RoutieService.java
+++ backend/spring-routie/src/main/java/routie/business/routie/application/RoutieService.java
@@ -38,7 +38,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
