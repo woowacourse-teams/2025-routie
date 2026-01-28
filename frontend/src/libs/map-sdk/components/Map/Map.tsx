647882b46c3e7080b2420e27a91eb1294e9976b6
AHHYUNJU <wndkgus0225@naver.com>
2026-01-28 21:21:42 +0900

    feat: Map 컴포넌트와 MarkerEngine 연결

--- frontend/src/libs/map-sdk/components/Map/Map.tsx
+++ frontend/src/libs/map-sdk/components/Map/Map.tsx
@@ -10,6 +10,7 @@ import {
 
 import KakaoMapContext from '../../contexts/KakaoMapContext';
 import { mapController } from '../../controllers/mapController';
+import { markerEngine } from '../../core/MarkerEngine';
 
 import type { MapProps } from './Map.types';
 import type { KakaoMap } from '../../../../../kakao.d';
@@ -177,6 +178,15 @@ const Map = forwardRef<HTMLDivElement, MapProps>(
         );
       };
     }, [map, onZoomChanged]);
+
+    // 마커 엔진에 map 인스턴스 설정
+    useEffect(() => {
+      markerEngine.setMap(map);
+
+      return () => {
+        markerEngine.setMap(null);
+      };
+    }, [map]);
 
     // Context 값 메모이제이션
     const contextValue = useMemo(() => ({ map }), [map]);
