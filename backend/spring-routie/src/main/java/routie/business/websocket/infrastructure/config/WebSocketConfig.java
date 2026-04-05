172e3cb1803058f44d13f7616855fbbc07330e14
goohong <goohong.chung@gmail.com>
2026-04-05 17:19:13 +0900

    feat: STOMP 엔드포인트 및 메시지 브로커 설정 추가

---
+++ backend/spring-routie/src/main/java/routie/business/websocket/infrastructure/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package routie.business.websocket.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Value("${websocket.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void configureMessageBroker(final MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic", "/queue");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(final StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/v1")
+                .setAllowedOriginPatterns(allowedOrigins)
+                .withSockJS();
+    }
+}
